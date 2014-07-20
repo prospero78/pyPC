b38ecfc726ccc13e046212c0894d32b8354230cb
prospero78 <prospero.78.su@gmail.com>
2014-07-20 20:24:52 +0300

    239 Теперь окно в духе Toplevel (clsWinAbout) создаётся в памяти и тихо там сидит, пока не позовут. )))

--- pakPC/pakGUI/pakWinAbout/modWinAbout.py
+++ pakPC/pakGUI/pakWinAbout/modWinAbout.py
@@ -8,3 +8,8 @@ from Tkinter import Toplevel
 class clsWinAbout(Toplevel):
     def __init__(self, root=None):
         self.root=root
+        Toplevel.__init__(self)
+        self.state('withdrawn')
+        
+    def show(self):
+        self.state('normal')
