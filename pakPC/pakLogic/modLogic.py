138d5cd9656983125831f15f4857d1ff38960d06
prospero78 <prospero.78.su@gmail.com>
2014-07-20 19:47:37 +0300

    229 Добавлены методы запуска и выхода из программы

--- pakPC/pakLogic/modLogic.py
+++ pakPC/pakLogic/modLogic.py
@@ -2,7 +2,13 @@
 '''
 Главный класс общей логики
 '''
-
+import sys
 class clsLogic:
     def __init__(self, root=None):
         self.root=None
+        
+    def run(self):
+        pass
+        
+    def exit(self):
+        sys.exit(0)
