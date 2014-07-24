cddf68577b8dd6b500125a540c8170c1608f6325
prospero78 <prospero.78.su@gmail.com>
2014-07-24 16:35:59 +0300

    247 Рефакторинг кода.

--- pakPC/pakGUI/pakWinAbout/modWinAbout.py
+++ pakPC/pakGUI/pakWinAbout/modWinAbout.py
@@ -7,20 +7,30 @@ from Tkinter import Toplevel, Frame, Button
 
 class clsWinAbout(Toplevel):
     def __init__(self, root=None):
-        self.root=root
-        Toplevel.__init__(self)
-        self.state('withdrawn')
-        self.title(self.root.Res.winAbout_name)
-        self.minsize(300, 200)
+        def create_self():
+            self.root=root
+            Toplevel.__init__(self)
+            self.state('withdrawn')
+            self.title(self.root.Res.winAbout_name)
+            self.minsize(300, 200)
         
-        self.frmUp=Frame(self, border=3, relief='groove')
-        self.frmUp.pack(fill='both', expand=1, side='top')
+        def create_frmUp():
+            self.frmUp=Frame(self, border=3, relief='groove')
+            self.frmUp.pack(fill='both', expand=1, side='top')
         
-        self.frmBtn=Frame(self, border=3, relief='raised')
-        self.frmBtn.pack(side='bottom', fill='x')
+        def create_frmBtn():
+            self.frmBtn=Frame(self, border=3, relief='raised')
+            self.frmBtn.pack(side='bottom', fill='x')
+            
+            self.btnCloseAbout=Button(self.frmBtn, text='Close About', bg='gray', command=self.destroy)
+            self.btnCloseAbout.pack(side='right')
         
-        self.btnCloseAbout=Button(self.frmBtn, text='Close About')
-        self.btnCloseAbout.pack(side='right')
+        
+        create_self()
+        create_frmUp()
+        create_frmBtn()
+        
+        
         
     def show(self):
         self.state('normal')
