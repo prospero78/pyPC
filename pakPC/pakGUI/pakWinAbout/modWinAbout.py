9aa6730882dee94c392f7554694df7f9e600f823
prospero78 <prospero.78.su@gmail.com>
2014-07-24 16:42:51 +0300

    250 Добавлена надпись с адресом проекта на GitHub'e

--- pakPC/pakGUI/pakWinAbout/modWinAbout.py
+++ pakPC/pakGUI/pakWinAbout/modWinAbout.py
@@ -3,12 +3,11 @@
 Класс окна "О программе".
 '''
 
-from Tkinter import Toplevel, Frame, Button
+from Tkinter import Toplevel, Frame, Button, Label
 
 class clsWinAbout(Toplevel):
     def __init__(self, root=None):
         def create_self():
-            self.root=root
             Toplevel.__init__(self)
             self.state('withdrawn')
             self.title(self.root.Res.winAbout_name)
@@ -17,6 +16,9 @@ class clsWinAbout(Toplevel):
         def create_frmUp():
             self.frmUp=Frame(self, border=3, relief='groove')
             self.frmUp.pack(fill='both', expand=1, side='top')
+            
+            self.lblGit=Label(self.frmUp, text=self.root.Res.winAbout_url, fg='blue', cursor='hand2')
+            self.lblGit.pack(side='bottom', fill='x')
         
         def create_frmBtn():
             self.frmBtn=Frame(self, border=3, relief='raised')
@@ -25,7 +27,7 @@ class clsWinAbout(Toplevel):
             self.btnCloseAbout=Button(self.frmBtn, text='Close About', bg='gray', command=self.destroy)
             self.btnCloseAbout.pack(side='right')
         
-        
+        self.root=root
         create_self()
         create_frmUp()
         create_frmBtn()
