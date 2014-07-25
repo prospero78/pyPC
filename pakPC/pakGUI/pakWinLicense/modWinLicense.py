aaaf0ddd82263772f53a4a49765f892103b1ac2b
prospero78 <prospero.78.su@gmail.com>
2014-07-25 14:35:04 +0300

    284 Добавлены фрейм и кнопки выбора языков лицензии

--- pakPC/pakGUI/pakWinLicense/modWinLicense.py
+++ pakPC/pakGUI/pakWinLicense/modWinLicense.py
@@ -3,7 +3,7 @@
 Класс окна "Лицензия".
 '''
 
-from Tkinter import Toplevel
+from Tkinter import Toplevel, Frame, Button
 
 class clsWinLicense(Toplevel):
     def __init__(self, root=None):
@@ -13,8 +13,18 @@ class clsWinLicense(Toplevel):
             self.minsize(350,240)
             self.state('withdrawn')
        
+        def create_frmBtn():
+            self.frmBtn=Frame(self, border=3, relief='raised')
+            self.frmBtn.pack(side='bottom', fill='x')
+            
+            self.btnEngLicence=Button(self.frmBtn, text='England', bg='gray', command=self.destroy)
+            self.btnEngLicence.pack(side='left')
+            
+            self.btnLocalLicense=Button(self.frmBtn, text='Russian', bg='gray', command=self.root.Control.show_winLicense)
+            self.btnLocalLicense.pack(side='left')
         self.root=root
         create_self()
+        create_frmBtn()
         
     def show(self):
         self.state('normal')
