a8c7a54bcb7487595d85d1668e716c64dda09ef0
prospero78 <prospero.78.su@gmail.com>
2014-07-25 13:42:42 +0300

    279 Добавлены процедуры скрытияи показа окна по лицензионным требованиям

--- pakPC/pakGUI/pakWinLicense/modWinLicense.py
+++ pakPC/pakGUI/pakWinLicense/modWinLicense.py
@@ -11,6 +11,18 @@ class clsWinLicense(Toplevel):
             Toplevel.__init__(self)
             self.title(self.root.Res.winLicense_title)
             self.minsize(350,240)
+            self.state('withdrawn')
        
         self.root=root
         create_self()
+        
+    def show(self):
+        self.state('normal')
+        # показать поверх всех с фокусом
+        self.focus_set()
+        self.grab_set()
+        self.wait_window()
+    
+    def destroy(self):
+        self.state('withdrawn')
+        self.grab_release()
