5af484b306a8c7d729872ecddbd417f02c00b063
prospero78 <prospero.78.su@gmail.com>
2014-07-25 09:50:19 +0300

    266 Добавлено определение класса окна лицензии

--- pakPC/pakGUI/pakWinLicense/modWinLicense.py
+++ pakPC/pakGUI/pakWinLicense/modWinLicense.py
@@ -2,3 +2,10 @@
 '''
 Класс окна "Лицензия".
 '''
+
+from Tkinter import Toplevel
+
+class clsWinLicense(Toplevel):
+    def __init__(self, root=none):
+        Toplevel.__init__(self)
+        self.title('License')
