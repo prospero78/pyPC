c4460b6ce446f968db3a37c608c355fea56df23c
prospero78 <prospero.78.su@gmail.com>
2014-08-01 16:42:44 +0300

    313 Добавлена процедура заливки экрана заданным цветом

--- pakPC/pakVideo/modVideo.py
+++ pakPC/pakVideo/modVideo.py
@@ -3,7 +3,13 @@
 Класс видеокарты.
 '''
 
-
 class clsVideo():
     def __init__(self, root=None):
         self.root=root
+        
+    
+    def fill(self, color='#000'):
+        '''
+        Заполнение экрана виртуального компьютера заданным цветом.
+        '''
+        pass
