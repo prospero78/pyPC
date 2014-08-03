4284b6b194fcb27ccb49eb47582cd8e29f9d3b90
prospero78 <prospero.78.su@gmail.com>
2014-08-03 20:51:44 +0300

    336 Добавление фрейма для отображения строк дисков

--- pakPC/pakGUI/pakWinIDC/modWinIDC.py
+++ pakPC/pakGUI/pakWinIDC/modWinIDC.py
@@ -3,7 +3,7 @@
 Класс окна интерфейса дискового кластера.
 '''
 
-from Tkinter import Toplevel
+from Tkinter import Toplevel, Frame, Button, Label
 
 class clsWinIDC(Toplevel):
     def __init__(self, root=None):
@@ -13,8 +13,45 @@ class clsWinIDC(Toplevel):
             self.title(self.root.Res.winIDC_name)
             self.minsize(350, 200)
         
+        def create_frmBtn():
+            self.frmBtn=Frame(self, border=2, relief='sunken')
+            self.frmBtn.pack(side='bottom', fill='x')
+            
+            self.btnCancel=Button(self.frmBtn, text='Cancel')
+            self.btnCancel.pack(side='right')
+            
+            self.btnReturn=Button(self.frmBtn, text='Return')
+            self.btnReturn.pack(side='right')
+            
+            self.btnOk=Button(self.frmBtn, text=' Ok ')
+            self.btnOk.pack(side='right')
+            
+            self.btnCreate=Button(self.frmBtn, text='Create')
+            self.btnCreate.pack(side='left')
+            
+            self.btnOpen=Button(self.frmBtn, text='Open')
+            self.btnOpen.pack(side='left')
+            
+        def create_frmIDC():
+            self.frmIDC=Frame(self, border=2, relief='sunken')
+            self.frmIDC.pack(side='top', fill='both', expand=1)
+            
+            self.lblDisk0=Label(self.frmIDC, text='./data/default.dsk', anchor='w', border=2, relief='groove')
+            self.lblDisk0.pack(side='top', fill='x')
+            
+            self.lblDisk1=Label(self.frmIDC, text='None', anchor='w', border=2, relief='groove')
+            self.lblDisk1.pack(side='top', fill='x')
+            
+            self.lblDisk2=Label(self.frmIDC, text='None', anchor='w', border=2, relief='groove')
+            self.lblDisk2.pack(side='top', fill='x')
+            
+            self.lblDisk3=Label(self.frmIDC, text='None', anchor='w', border=2, relief='groove')
+            self.lblDisk3.pack(side='top', fill='x')
+            
         self.root=root
         create_self()
+        create_frmBtn()
+        create_frmIDC()
     
     def show(self):
         self.state('normal')
