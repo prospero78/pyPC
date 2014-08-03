a13e442ec764bbc4ae9c009e5ba407de89e745e1
prospero78 <prospero.78.su@gmail.com>
2014-08-03 21:04:29 +0300

    337 Добавлен виджет для отображения диска (выбора и создания)

---
+++ pakPC/pakGUI/pakWidgets/modFrmDiskSelect.py
@@ -0,0 +1,10 @@
+# -*- coding: utf8 -*-
+'''
+Инициализация пакета виджета выбора диска.
+'''
+
+from Tkinter import Frame, Label, Button
+
+class clsFrmDiskSelect(Frame):
+    def __init__(self):
+        pass
