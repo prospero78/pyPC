c420177f1f893e1a93d4cc3ed9fb58eefbc94228
prospero78 <prospero.78.su@gmail.com>
2014-08-03 21:35:04 +0300

    344 Добавлен параметр по умолчанию при создании класса (path)

--- pakPC/pakGUI/pakWidgets/modFrmDiskSelect.py
+++ pakPC/pakGUI/pakWidgets/modFrmDiskSelect.py
@@ -6,15 +6,15 @@
 from Tkinter import Frame, Label, Button
 
 class clsFrmDiskSelect(Frame):
-    def __init__(self, root=None, text='Disk-0:'):
+    def __init__(self, root=None, text='Disk-0:', path='./data/default.dsk'):
         def create_self():
             Frame.__init__(self, master=self.root)
+            self.pack(side='top', fill='x')
             
             self.lblPath=Label(self, text=self.text, border=2, relief='raised')
             self.lblPath.pack(side='left')
             
-            
-            self.lblDiskPath=Label(self, text='./data/default.dsk', anchor='w', border=2, relief='groove')
+            self.lblDiskPath=Label(self, text=self.path, anchor='w', border=2, relief='groove')
             self.lblDiskPath.pack(side='left', fill='x', expand=1)
             
             self.btnCreate=Button(self, text='Create')
@@ -23,7 +23,11 @@ class clsFrmDiskSelect(Frame):
             self.btnOpen=Button(self, text='Open')
             self.btnOpen.pack(side='left')
             
-            self.pack(side='top', fill='both', expand=1)
+            self.btnClear=Button(self, text='Clear')
+            self.btnClear.pack(side='left')
+            
+            
         self.root=root
+        self.path=path
         self.text=text
         create_self()
