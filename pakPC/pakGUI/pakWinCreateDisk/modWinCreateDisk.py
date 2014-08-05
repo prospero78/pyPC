77b7fa3c19a518f5c40ecb39ec7aba74ca1df9c1
prospero78 <prospero.78.su@gmail.com>
2014-08-05 15:41:27 +0300

    0.383 Добавлен фрейм для отображения параметров создаваемого диска, кнопка "Отмена"

--- pakPC/pakGUI/pakWinCreateDisk/modWinCreateDisk.py
+++ pakPC/pakGUI/pakWinCreateDisk/modWinCreateDisk.py
@@ -11,7 +11,7 @@ class clsWinCreateDisk(Toplevel):
             Toplevel.__init__(self)
             self.state('withdrawn')
             self.title(self.root.Res.winCreateDisk_title)
-            #self.minsize(640, 480)
+            self.minsize(350, 200)
             
         def create_frmBtn():
             self.frmBtn=Frame(self, border=2, relief='sunken')
@@ -19,10 +19,18 @@ class clsWinCreateDisk(Toplevel):
             
             self.btnOk=Button(self.frmBtn, text=' Ok ')#, command=self.root.Control.winIDC_ok)
             self.btnOk.pack(side='right')
+            
+            self.btnCancel=Button(self.frmBtn, text='Cancel')#, command=self.root.Control.winIDC_cancel)
+            self.btnCancel.pack(side='right')
         
+        def create_frmDiskParam():
+            self.frmDiskParam=Frame(self, border=2, relief='sunken')
+            self.frmDiskParam.pack(side='top', fill='both', expand=1)
+            
         self.root=root
         create_self()
         create_frmBtn()
+        create_frmDiskParam()
         
     def show(self):
         self.state('normal')
