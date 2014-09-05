c2006376099e428d3e223348b3dc7764afeb2c21
prospero78 <prospero.78.su@gmail.com>
2014-09-05 15:36:44 +0300

    0.446 Добавлена команда правый сдвиг на 1 бит для регистра А из ячейки памяти.

--- pakPC/pakCPU/cmodCPU.pyx
+++ pakPC/pakCPU/cmodCPU.pyx
@@ -55,6 +55,7 @@ if True:
     DEF A_xor_nn=24
     DEF A_or_nn =25
     DEF A_and_nn=26
+    DEF A_shr_nn=27
     
     
 
@@ -397,6 +398,17 @@ cdef class clsCPU:
         elif cop==A_and_nn:      # логическое И значение регистра А из ячейки с адресом nn
             nn=self.Mem.adr[self.PC.val+1] # получить адрес nn, в котором лежит число
             self.RegA.val&=self.Mem.adr[nn] # логическое ИЛИ
+            if self.RegA.val==0:
+                self.RegA.FlagZ=1
+            else:
+                self.RegA.FlagZ=0
+            self.RegA.FlagO=0
+            self.RegA.FlagS=0
+            self.RegA.FlagC=0
+            self.PC.val+=2
+        elif cop==A_shr_nn:      # правый сдвиг значения регистра А из ячейки с адресом nn
+            nn=self.Mem.adr[self.PC.val+1] # получить адрес nn, в котором лежит число
+            self.RegA.val>>self.Mem.adr[nn] # правый сдвиг
             if self.RegA.val==0:
                 self.RegA.FlagZ=1
             else:
