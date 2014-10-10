636d3fc9a8230fffce76751999dc93d62192c66a
prospero78 <prospero.78.su@gmail.com>
2014-10-10 12:10:39 +0300

    0.562 Переопределён БИОС.

--- pakPC/pakResurs/modBios.py
+++ pakPC/pakResurs/modBios.py
@@ -2,11 +2,14 @@
 '''
 модуль определяет БИОС виртуальной машины.
 '''
-bios=[
-0, # A.rset(10)
-10, # n=10
-1,  # A_radd(1)
-1, # n=1
-42, # A_jmp(0)
-0, m=0
-]
+class clsBios:
+    def __init__(self, root=None):
+        self.root=root
+        self.data={
+            0:0, # A.rset(10)
+            1:10, # n=10
+            2:1,  # A_radd(1)
+            3:1,  # n=1
+            4:42, # A_jmp(0)
+            5:0,  # m=0
+            }
