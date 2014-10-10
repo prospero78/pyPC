ab522cba962148c8d68b58d8d4d38c2f112d9b78
prospero78 <prospero.78.su@gmail.com>
2014-10-10 09:57:13 +0300

    0.540 Добавлен питоний файл для видеокарты.

--- pakPC/pakVideo/modVideo.py
+++ pakPC/pakVideo/modVideo.py
@@ -1,12 +1,33 @@
 # -*- coding: utf8 -*-
 '''
 Класс видеокарты.
+Видеокарта приписана жёстко к портам.
+0 -- порт запросов от CPU
+1 -- координата х
+2 -- координата y
+3 -- монохром (установить/сбросить)
+
+Команды запроса:
+1 -- установить режим
+    (
 '''
 
-class clsVideo():
+cdef struct VideoMemory:
+    char adr[3888000] # блок видеопамяти рассчитанный на 1024х768х24 бита
+
+cdef struct BufChar:
+    char buf[1024] # строковый буфер для работы с экраном
+
+
+cdef class clsVideo:
+    cdef VideoMemory *Mem # ссылка на структуру видеопамяти
+    cdef int mode # регистр отвечающий за режим видеокарты
+    cdef int command # регистр типа команды
+    cdef BufChar *buf  # строковый буфер для работы с экраном
+    
     def __init__(self, root=None):
         self.root=root
-        
+        self.mode=0
     
     def fill(self, color='#000'):
         '''
