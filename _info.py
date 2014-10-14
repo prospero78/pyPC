94e5fd5ae2eeb125ff08ef0ad4359bb76775fd61
prospero78 <prospero.78.su@gmail.com>
2014-10-14 16:51:47 +0300

    0.622 ADD: добавлен модуль оценивающий состояние проекта. )

---
+++ _info.py
@@ -0,0 +1,29 @@
+# -*- coding: utf8 -*-
+
+class clsInfo:
+
+   def __init__(self):
+    self.authorInfo='Valerij Shipkov'
+    self.projectInfo=''
+    self.aboutInfo=''
+    self.buildInfo=''
+
+  def author (self):
+
+    return self.authorInfo
+
+  def project (self):
+
+    return self.projectInfo
+
+  def about(self):
+
+    return self.aboutInfo
+
+  def version (self):
+
+    return self.projectInfo
+
+  def build(self):
+
+    return self.buildInfo
