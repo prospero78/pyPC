c14d257f92bef36752de44c0153356f295f47f47
prospero78 <prospero.78.su@gmail.com>
2014-10-15 20:05:49 +0300

    0.650 FIX: пока модуль компилируемой памяти оставил как простой python-код. Просто за счёт вызовов памяти приподнять скороть. На домашнем ноуте всего 352 кГц.

--- pakPC/pakCPU/pakMem/cmodMemory.pyx
+++ pakPC/pakCPU/pakMem/cmodMemory.pyx
@@ -2,14 +2,11 @@
 '''
 Класс памяти с компиляцией. Будем со временем дорабатывать.
 '''
-cdef struct Mem: # описание структуры памяти
-    int adr[16777216] # 2**24
-    int max_adr
 
-cdef class clsMemory:
-    cdef public Mem *Mem           # память виртуального компьютера
-    def __init__(self, max_adr=16777216):
-        self.Mem.max_adr=max_adr
+class clsMemory:
+    def __init__(self, max_adr=2**20):
+        self.max_adr=max_adr
         # инициализация памяти виртуального компьютера
-        for i in xrange(0,self.Mem.max_adr):
-            self.Mem.adr[i]=0
+        self.adr={}
+        for i in xrange(0,self.max_adr):
+            self.adr[i]=0
