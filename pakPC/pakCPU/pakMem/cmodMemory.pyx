664d58464bf50f3ce79988155066fa0ef3c12b71
prospero78 <prospero.78.su@gmail.com>
2014-10-15 21:28:48 +0300

    0.655 ADD: переделан и добавлен компилируемый модуль.

--- pakPC/pakCPU/pakMem/cmodMemory.pyx
+++ pakPC/pakCPU/pakMem/cmodMemory.pyx
@@ -3,11 +3,22 @@
 Класс памяти с компиляцией. Будем со временем дорабатывать.
 '''
 
-class clsMemory:
-    def __init__(self, int max_adr=2**20):
+cdef class clsMemory:
+    cdef int adr[16777216] # 2**24 16777216
+    cdef int max_adr
+    def __init__(self, long max_adr=2**24):
         self.max_adr=max_adr
+        #cdef int adr[2**24] # 2**24
         # инициализация памяти виртуального компьютера
-        self.adr={}
-        cdef int i
+        cdef long i
         for i in xrange(0, max_adr):
             self.adr[i]=0
+            
+    cpdef int get_adr(self, int adr):
+        return self.adr[adr]
+        
+    cpdef int set_adr(self, int adr):
+        return self.adr[adr]
+        
+    cpdef int get_max_adr(self):
+        return self.max_adr
