08a455b4f9672ad72f2de48a882b2e0985b29a5e
prospero78 <prospero.78.su@gmail.com>
2014-10-15 15:26:36 +0300

    0.639 ADD: добавлен фрейм для отображения частоты работы процессора.

---
+++ pakPC/pakGUI/pakWidgets/modFrmCpuFrec.py
@@ -0,0 +1,11 @@
+# -*- coding: utf8 -*-
+'''
+Модуль отображения текущей частоты работы процессора.
+Довольно важная вещь. )
+'''
+
+from modFrmKeyValue import clsFrmKeyValue
+
+class clsFrmCpuFrec(clsFrmKeyValue):
+    def __init__(self, root=None):
+        clsFrmKeyValue.__init__(self, root=root, key='Frec', value='0 Hz')
