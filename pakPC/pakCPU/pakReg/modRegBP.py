e27e7ab2b06e367b9e9d1e7250dc7bc82dc8dc71
prospero78 <prospero.78.su@gmail.com>
2014-10-16 14:55:03 +0300

    0.708 FIX: удаление лишних переменных и параметров.

--- pakPC/pakCPU/pakReg/modRegBP.py
+++ pakPC/pakCPU/pakReg/modRegBP.py
@@ -4,13 +4,12 @@
 '''
 
 class clsRegBP:
-    def __init__(self, root=None, adr_break=0, act=0, adr_proc=0):
+    def __init__(self, adr_break=0, act=0, adr_proc=0):
         '''
         adr_break -- aдрес, на который бряк
         act -- активность регистра
         adr_proc -- куда перекинуть управление
         '''
-        self.root=root
         self.adr_break=adr_break
         self.adr_old=0
         self.act=act
