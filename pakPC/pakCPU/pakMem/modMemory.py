695fa854c72c5bbb2ed7fe8bf5e76eaf61cbc302
prospero78 <prospero.78.su@gmail.com>
2014-10-16 11:37:12 +0300

    0.682 ADD: удалена root-ссылка (незачем), добалены методы для совместимости с бинарным модулем.

--- pakPC/pakCPU/pakMem/modMemory.py
+++ pakPC/pakCPU/pakMem/modMemory.py
@@ -4,10 +4,18 @@
 '''
 
 class clsMemory:
-    def __init__(self, root=None, max_adr=2**20):
-        self.root=root
+    def __init__(self, max_adr=2**22):
         self.max_adr=max_adr
         # инициализация памяти виртуального компьютера
         self.adr={}
         for i in xrange(0,self.max_adr):
             self.adr[i]=0
+    
+    def get_adr(self, adr):
+        return self.adr[adr]
+        
+    def set_adr(self, adr, val):
+        self.adr[adr]=val
+        
+    def get_max_adr(self):
+        return self.max_adr
