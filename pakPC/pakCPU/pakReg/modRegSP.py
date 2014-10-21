2dd9571501d49c9a7f2e051a8a0717e81f5a5ff7
prospero78 <prospero.78.su@gmail.com>
2014-10-21 11:14:04 +0300

    0.751 ADD: добавлено описание, пофиксены параметры создания класса.

--- pakPC/pakCPU/pakReg/modRegSP.py
+++ pakPC/pakCPU/pakReg/modRegSP.py
@@ -1,9 +1,14 @@
 # -*- coding: utf8 -*-
 '''
 Класс указателя стека.
+
+val -- максимальное значение регистра. 
+Устанавливается по количеству максимальной допустимой памяти.
+
+min_adr -- дно стека.
 '''
 
 class clsRegSP:
-    def __init__(self, val=2**24, min_adr=0):
+    def __init__(self, val=None, min_adr=None):
         self.val=val
         self.min_adr=min_adr
