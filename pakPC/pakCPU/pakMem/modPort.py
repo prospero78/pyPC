154067e2bc744789e1e845efb4e51abcff94fe80
prospero78 <prospero.78.su@gmail.com>
2014-10-21 10:32:04 +0300

    0.741 ADD: добавлен клас портов. Как он будет работать -- пока боьлше вопросов, чем ответов.

---
+++ pakPC/pakCPU/pakMem/modPort.py
@@ -0,0 +1,27 @@
+# -*- coding: utf8 -*-
+'''
+Класс описывающий массив портов.
+Не вижу смысла задавать портов более,
+чем 65 тыс. (2**16)
+'''
+
+class clsPort:
+    def __init__(self, max_port=2**16):
+        # максимально допустимое количество портов
+        self.max_port=max_port
+        # инициализация адресов портов виртуального компьютера
+        self.adr={}
+        for i in xrange(0, self.act_mem):
+            self.adr[i]=0
+    
+    def get_adr(self, adr):
+        '''
+        Получить состояние порта
+        '''
+        return self.adr[adr]
+        
+    def set_adr(self, adr, val):
+        '''
+        Установить состояние порта
+        '''
+        self.adr[adr]=val
