470c8886198df57bc9db8299e687fe3182c3f5d0
prospero78 <prospero.78.su@gmail.com>
2014-10-28 22:20:43 +0200

    0.976 FIX: исправление потенциальных проблем, по рекомендациям PyLint. Качество кода 9.73! )

--- pak_pc/pak_resurs/mod_resurs.py
+++ pak_pc/pak_resurs/mod_resurs.py
@@ -3,8 +3,7 @@
 Класс ресурсов. Содержит всякие полезные штуки для интернационализации,
 графики и т. д.
 '''
-from pak_pc.pak_resurs.mod_lang import ClsLang
-class ClsRes(ClsLang):
+class ClsRes(object):
     '''
     Класс создаёт все необходимые ресурсы в программе.
     '''
@@ -12,14 +11,18 @@ class ClsRes(ClsLang):
         self.root = root
         self.lang = lang
         self.arg = arg
+        self.build = '0.975' # текущая версия сборки
 
-        self.create_res()
         self.pars_arg()
         if self.lang == 'ru':
             from pak_pc.pak_resurs.mod_lang_ru import ClsLangRu
-            ClsLangRu.__init__(self, lang=self.lang)
+            self.lang_str = ClsLangRu(lang=self.lang)
 
     def pars_arg(self):
+        '''
+        Осуществляет парсинг аргументов передаваемых при запуске
+        программы.
+        '''
         if len(self.arg) > 1:
             for i in self.arg:
                 if ('--help' in i) or ('-h' in i) or ('/h' in i):
@@ -30,7 +33,7 @@ class ClsRes(ClsLang):
     --about (-a)     о программе
                     '''.decode('utf8')
                 elif ('--about' in i) or ('-a' in i):
-                    print '''pyPC vers. ''' + self.vers + '''
+                    print '''pyPC build. ''' + self.build + '''
 О программе:
     Эта программа представляет собой вымышленный
     (но вполне рабочий) виртуальный компьютер.
@@ -44,14 +47,3 @@ class ClsRes(ClsLang):
     (A) Valeriy Shipkov, 2014
     (L) GNU GPL v.3
                     '''.decode('utf8')
-
-    def create_res(self):
-        '''
-        vers -- версия сборки проекта, xxx -- версия сборки
-        0.xxx -- черновая версия
-        1.ххх -- зарелизенная версия
-        И других вариантов не будет.
-        '''
-        self.vers = '0.870' # текущая версия сборки
-        self.max_adr = 2**16 # максимальный адрес памяти
-        self.max_reg_val = 2**16 # максимальное значение регистра
