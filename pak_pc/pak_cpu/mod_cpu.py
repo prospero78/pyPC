2b6e4a132646f481c718b35a29fdf9b734a5378f
prospero78 <prospero.78.su@gmail.com>
2014-10-28 23:33:07 +0200

    0.1047 FIX: исправления по рекомендации PyLint.

--- pak_pc/pak_cpu/mod_cpu.py
+++ pak_pc/pak_cpu/mod_cpu.py
@@ -19,13 +19,14 @@ from pakReg.modRegBP import clsRegBP
 
 class ClsCPU(multiprocessing.Process):
     '''
-        Здесь надо хорошо подумать сколько элементарных операций будет выполнять процессор.
-        Длина команды (без старшего бита) ограничена семью битами.
-        Видимо, пока буду пилить один РОН, т. к. непонятно, сколько будет элементарных команд
-        на один регистр. Теоретически, и одного регистра должно хватить с удовольствием. )
+        Здесь надо хорошо подумать сколько элементарных операций будет
+        выполнять процессор. Длина команды (без старшего бита)
+        ограничена семью битами. Видимо, пока буду пилить один РОН, т.
+        к. непонятно, сколько будет элементарных команд на один регистр.
+        Теоретически, и одного регистра должно хватить с удовольствием.)
         ----------------------
-        Класс центрального процессора обеспечивает выполнение кодов операций и 
-        обработку данных.
+        Класс центрального процессора обеспечивает выполнение кодов
+        операций и обработку данных.
         ----------------------
         FEDCBA98 7 6543210
                  |   |
@@ -33,10 +34,10 @@ class ClsCPU(multiprocessing.Process):
                  два регистра общего назначения
            векторы прерываний
         расширенные команды процессора
-        
+
         На каждый регистр отводится по 128 простых команд (7 бит)
     '''
-    
+
     def __init__(self, max_value=0, max_adr=0, vcom=None, vinfo=None):
         def load_bios():
             '''
@@ -58,17 +59,17 @@ class ClsCPU(multiprocessing.Process):
         self.qcom = multiprocessing.Queue()
         # очередь для отправки информации
         self.qinfo = multiprocessing.Queue()
-        
+
         # частота работы процессора
         self.frec = 1.0
         # количество команд для замера
         self.time_code = 50000
         # признак необходимости цикла отладки
         self.run_debug = 0
-        
+
         self.max_val = max_value
         self.max_adr = max_adr
-       
+
         # инициализация памяти
         self.Mem = clsMemory()
         load_bios()
@@ -76,24 +77,23 @@ class ClsCPU(multiprocessing.Process):
         self.Port = clsPort(max_port=2**16,
                             vinfo=vinfo,
                             vcom=vcom)
-        
+
         self.RegSP = clsRegSP(val=self.Mem.act_mem-1,
-                            min_adr=self.Mem.max_adr-101)
+                              min_adr=self.Mem.max_adr-101)
         self.RegPC = clsRegPC(val=0, max_adr=self.RegSP.min_adr-1)
-        
+
         # регистр для установки принудительного прерывания исполнения программы
         self.RegBP = clsRegBP(act=0, adr_break=0, adr_proc=0)
-        
+
         self.RegA = clsReg(root=self,
-                        mem=self.Mem,
-                        pc=self.RegPC,
-                        sp=self.RegSP,
-                        port=self.Port)
+                           mem=self.Mem,
+                           pc=self.RegPC,
+                           sp=self.RegSP,
+                           port=self.Port)
     def run(self):
         '''
         Метод необходим для запуска отдельного процесса.
         '''
-       
         while True:
             #print("The process CPU!")
             if not self.qcom.empty():
@@ -102,11 +102,12 @@ class ClsCPU(multiprocessing.Process):
                     com = com['com']
                     if com == 'step()':
                         self.RegA.command()
-                        #print 'com:step()    RegBP.adr_break=', self.RegBP.adr_break
+                        #print 'com:step()    RegBP.adr_break=',
+                        # self.RegBP.adr_break
                         self.send_info()
                         #print '***'
                     elif com == 'debug(on)':
-                        self.run_debug=1
+                        self.run_debug = 1
                         info = {'debug':'on'}
                         self.qinfo.put(info)
                         self.debug()
@@ -124,21 +125,21 @@ class ClsCPU(multiprocessing.Process):
                     self.RegBP.act = reg['act']
                     self.RegBP.adr_proc = reg['adr_proc']
                     self.RegBP.adr_break = reg['adr_break']
-                    RegBP = { 'act':self.RegBP.act,
-                                'adr_proc':self.RegBP.adr_proc,
-                                'adr_break':self.RegBP.adr_break}
+                    RegBP = {'act':self.RegBP.act,
+                             'adr_proc':self.RegBP.adr_proc,
+                             'adr_break':self.RegBP.adr_break}
                     info = {'RegBP':RegBP}
                     self.qinfo.put(info)
                     pass
             sleep(0.1)
-        
+
     def debug(self):
         i = 0
         while self.run_debug == 1:
             time1 = time()
             while i < self.time_code:
                 self.RegA.command()
-                i += 1;
+                i += 1
             else:
                 i = 0
                 dtime = time() - time1
@@ -149,7 +150,7 @@ class ClsCPU(multiprocessing.Process):
                     com = self.qcom.get()
                     if com.has_key('com'):
                         com = com['com']
-                        if com=='debug(off)':
+                        if com == 'debug(off)':
                             self.run_debug = 0
                             info = {'debug':'off'}
                             self.qinfo.put(info)
@@ -160,7 +161,7 @@ class ClsCPU(multiprocessing.Process):
         else:
             info = {'debug':'end'}
             self.qinfo.put(info)
-    
+
     def reset_pc(self):
         '''
         Принудительно сбрасывает текущее состояние компьютера.
@@ -173,19 +174,19 @@ class ClsCPU(multiprocessing.Process):
         self.RegPC.val = 0
         self.RegSP.val = 0
         self.send_info()
-    
+
     def send_info(self):
-        RegA = {  'val':self.RegA.val,
+        RegA = {'val':self.RegA.val,
                 'FlagZ':self.RegA.FlagZ,
                 'FlagO':self.RegA.FlagO,
                 'FlagC':self.RegA.FlagC}
         RegPC = {'val':self.RegPC.val}
-        RegBP = { 'act':self.RegBP.act,
-                'adr_proc':self.RegBP.adr_proc,
-                'adr_break':self.RegBP.adr_break}
-        RegSP = { 'adr':self.RegSP.val,
-                'val':self.Mem.adr[self.RegSP.val]}
-        info = {  'RegA':RegA,
+        RegBP = {'act':self.RegBP.act,
+                 'adr_proc':self.RegBP.adr_proc,
+                 'adr_break':self.RegBP.adr_break}
+        RegSP = {'adr':self.RegSP.val,
+                 'val':self.Mem.adr[self.RegSP.val]}
+        info = {'RegA':RegA,
                 'RegPC' :RegPC,
                 'RegBP' :RegBP,
                 'RegSP':RegSP,}
