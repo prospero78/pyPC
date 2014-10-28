b43d02241e6b041d8cc26bd5feafc300efb88956
prospero78 <prospero.78.su@gmail.com>
2014-10-28 23:24:55 +0200

    0.1045/1946 FIX: исправления по рекомендации PyLint.

--- pak_pc/pak_cpu/mod_cpu.py
+++ pak_pc/pak_cpu/mod_cpu.py
@@ -47,44 +47,44 @@ class ClsCPU(multiprocessing.Process):
             from pakPC.pakResurs.modBios import bios
             for i in bios:
                 #print i, bios[i], type(i)
-                if i>self.Mem.max_adr:
+                if i > self.Mem.max_adr:
                     self.Mem.add_adr()
                 self.Mem.adr[i] = bios[i]
             print '  = BIOS load OK ='
         # создание отдельного процесса
         multiprocessing.Process.__init__(self)
-        self.daemon=True
+        self.daemon = True
         # очередь для получения команд
-        self.qcom=multiprocessing.Queue()
+        self.qcom = multiprocessing.Queue()
         # очередь для отправки информации
-        self.qinfo=multiprocessing.Queue()
+        self.qinfo = multiprocessing.Queue()
         
         # частота работы процессора
-        self.frec=1.0
+        self.frec = 1.0
         # количество команд для замера
-        self.time_code=50000
+        self.time_code = 50000
         # признак необходимости цикла отладки
-        self.run_debug=0
+        self.run_debug = 0
         
-        self.max_val=max_value
-        self.max_adr=max_adr
+        self.max_val = max_value
+        self.max_adr = max_adr
        
         # инициализация памяти
-        self.Mem=clsMemory()
+        self.Mem = clsMemory()
         load_bios()
          # инициализация портов
-        self.Port=clsPort(max_port=2**16,
+        self.Port = clsPort(max_port=2**16,
                             vinfo=vinfo,
                             vcom=vcom)
         
-        self.RegSP=clsRegSP(val=self.Mem.act_mem-1,
+        self.RegSP = clsRegSP(val=self.Mem.act_mem-1,
                             min_adr=self.Mem.max_adr-101)
-        self.RegPC=clsRegPC(val=0, max_adr=self.RegSP.min_adr-1)
+        self.RegPC = clsRegPC(val=0, max_adr=self.RegSP.min_adr-1)
         
         # регистр для установки принудительного прерывания исполнения программы
-        self.RegBP=clsRegBP(act=0, adr_break=0, adr_proc=0)
+        self.RegBP = clsRegBP(act=0, adr_break=0, adr_proc=0)
         
-        self.RegA=clsReg(root=self,
+        self.RegA = clsReg(root=self,
                         mem=self.Mem,
                         pc=self.RegPC,
                         sp=self.RegSP,
@@ -97,68 +97,68 @@ class ClsCPU(multiprocessing.Process):
         while True:
             #print("The process CPU!")
             if not self.qcom.empty():
-                com=self.qcom.get()
+                com = self.qcom.get()
                 if com.has_key('com'):
-                    com=com['com']
-                    if com=='step()':
+                    com = com['com']
+                    if com == 'step()':
                         self.RegA.command()
                         #print 'com:step()    RegBP.adr_break=', self.RegBP.adr_break
                         self.send_info()
                         #print '***'
-                    elif com=='debug(on)':
+                    elif com == 'debug(on)':
                         self.run_debug=1
-                        info={'debug':'on'}
+                        info = {'debug':'on'}
                         self.qinfo.put(info)
                         self.debug()
-                    elif com=='debug(off)':
-                        self.run_debug=0
-                        info={'debug':'off'}
+                    elif com == 'debug(off)':
+                        self.run_debug = 0
+                        info = {'debug':'off'}
                         self.qinfo.put(info)
-                    elif com=='reset':
+                    elif com == 'reset':
                         self.reset_pc()
                         self.reset_pc()
-                    elif com=='get_info()':
+                    elif com == 'get_info()':
                         self.send_info()
                 elif com.has_key('RegBP'):
-                    reg=com['RegBP']
-                    self.RegBP.act=reg['act']
-                    self.RegBP.adr_proc=reg['adr_proc']
-                    self.RegBP.adr_break=reg['adr_break']
-                    RegBP={ 'act':self.RegBP.act,
+                    reg = com['RegBP']
+                    self.RegBP.act = reg['act']
+                    self.RegBP.adr_proc = reg['adr_proc']
+                    self.RegBP.adr_break = reg['adr_break']
+                    RegBP = { 'act':self.RegBP.act,
                                 'adr_proc':self.RegBP.adr_proc,
                                 'adr_break':self.RegBP.adr_break}
-                    info={'RegBP':RegBP}
+                    info = {'RegBP':RegBP}
                     self.qinfo.put(info)
                     pass
             sleep(0.1)
         
     def debug(self):
-        i=0
-        while self.run_debug==1:
-            time1=time()
-            while i<self.time_code:
+        i = 0
+        while self.run_debug == 1:
+            time1 = time()
+            while i < self.time_code:
                 self.RegA.command()
-                i+=1;
+                i += 1;
             else:
-                i=0
-                dtime=time()-time1
-                inf_time={'dtime':dtime}
+                i = 0
+                dtime = time() - time1
+                inf_time = {'dtime':dtime}
                 self.qinfo.put(inf_time)
-                time1=time()
+                time1 = time()
                 if not self.qcom.empty():
-                    com=self.qcom.get()
+                    com = self.qcom.get()
                     if com.has_key('com'):
-                        com=com['com']
+                        com = com['com']
                         if com=='debug(off)':
-                            self.run_debug=0
-                            info={'debug':'off'}
+                            self.run_debug = 0
+                            info = {'debug':'off'}
                             self.qinfo.put(info)
-                        elif com=='reset':
+                        elif com == 'reset':
                             self.reset_pc()
-                            info={'debug':'reset'}
+                            info = {'debug':'reset'}
                             self.qinfo.put(info)
         else:
-            info={'debug':'end'}
+            info = {'debug':'end'}
             self.qinfo.put(info)
     
     def reset_pc(self):
@@ -166,26 +166,26 @@ class ClsCPU(multiprocessing.Process):
         Принудительно сбрасывает текущее состояние компьютера.
         Пока без сброса RegBP, не подчищает стек.
         '''
-        self.RegA.val=0
-        self.RegA.FlagZ=1
-        self.RegA.FlagO=0
-        self.RegA.FlagC=0
-        self.RegPC.val=0
-        self.RegSP.val=0
+        self.RegA.val = 0
+        self.RegA.FlagZ = 1
+        self.RegA.FlagO = 0
+        self.RegA.FlagC = 0
+        self.RegPC.val = 0
+        self.RegSP.val = 0
         self.send_info()
     
     def send_info(self):
-        RegA={  'val':self.RegA.val,
+        RegA = {  'val':self.RegA.val,
                 'FlagZ':self.RegA.FlagZ,
                 'FlagO':self.RegA.FlagO,
                 'FlagC':self.RegA.FlagC}
-        RegPC={'val':self.RegPC.val}
-        RegBP={ 'act':self.RegBP.act,
+        RegPC = {'val':self.RegPC.val}
+        RegBP = { 'act':self.RegBP.act,
                 'adr_proc':self.RegBP.adr_proc,
                 'adr_break':self.RegBP.adr_break}
-        RegSP={ 'adr':self.RegSP.val,
+        RegSP = { 'adr':self.RegSP.val,
                 'val':self.Mem.adr[self.RegSP.val]}
-        info={  'RegA':RegA,
+        info = {  'RegA':RegA,
                 'RegPC' :RegPC,
                 'RegBP' :RegBP,
                 'RegSP':RegSP,}
