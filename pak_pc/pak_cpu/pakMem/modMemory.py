aaf80d62da540d6d415ef73ccebdd52f26a2f841
prospero78 <prospero.78.su@gmail.com>
2014-10-28 23:02:51 +0200

    0.1018 UPD: исправление имён по рекомендации PyLint.

---
+++ pak_pc/pak_cpu/pakMem/modMemory.py
@@ -0,0 +1,30 @@
+# -*- coding: utf8 -*-
+'''
+Класс памяти. Будем со временем дорабатывать.
+'''
+
+class clsMemory:
+    def __init__(self, max_adr=2**22):
+        # максимально допустимое количество памяти
+        self.max_adr=max_adr
+        # текущее количество памяти
+        self.act_mem=1024
+        # инициализация памяти виртуального компьютера
+        self.adr={}
+        for i in xrange(0, self.act_mem):
+            self.adr[i]=0
+    
+    def get_adr(self, adr):
+        return self.adr[adr]
+        
+    def set_adr(self, adr, val):
+        self.adr[adr]=val
+        
+    def get_max_adr(self):
+        return self.max_adr
+        
+    def add_memory(self):
+        if self.act_mem<self.max_adr-1024:
+            for i in xrange(self.act_mem, self.act_mem+1024):
+                self.adr[i]=0
+            self.act_mem+=1024
