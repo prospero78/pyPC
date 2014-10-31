c5b8f4187dd4e6f8a7a8f70c10334c2c2cfbb274
prospero78 <prospero.78.su@gmail.com>
2014-10-31 15:06:22 +0200

    0.1191 FIX: исправление имён импорта и методов (рекомендация PyLint).

--- pak_pc/pak_controller/mod_controller.py
+++ pak_pc/pak_controller/mod_controller.py
@@ -3,74 +3,74 @@
 Главный класс контроллера
 '''
 
-class clsController:
+class ClsController:
     def __init__(self, root=None):
         self.root=root
         
     def reset_pc(self, event=''):
-        print  'clsController.reset_pc()'
-        self.Logic.reset_pc()
+        print  '.ClsController.reset_pc()'
+        self.logic.reset_pc()
         
     def winEditBP_hide(self, event=''):
-        #print  'clsController.winEditBP_hide()'
-        self.Logic.winEditBP_hide()
+        #print  '.ClsController.winEditBP_hide()'
+        self.logic.winEditBP_hide()
         
     def show_winEditBP(self, event=''):
-        #print  'clsController.show_winEditBP()'
-        self.Logic.show_winEditBP()
+        #print  '.ClsController.show_winEditBP()'
+        self.logic.show_winEditBP()
         
     def winMain_debug(self, event=''):
-        #print  'clsController.winMain_debug()'
-        self.Logic.debug_CPU()
+        #print  '.ClsController.winMain_debug()'
+        self.logic.debug_CPU()
         
     def win_main_step_cpu(self, event=''):
-        #print  'clsController.win_main_step_cpu()'
-        self.Logic.step_CPU()
+        #print  '.ClsController.win_main_step_cpu()'
+        self.logic.step_CPU()
     
     def win_create_disk_ok(self, event=''):
-        #print 'clsController.win_create_disk_ok()'
-        self.Logic.generate_new_disk()
+        #print '.ClsController.win_create_disk_ok()'
+        self.logic.generate_new_disk()
     
     def create_new_disk(self, event=None):
-        #print 'clsController.create_new_disk()'
-        self.Logic.create_new_disk()
+        #print '.ClsController.create_new_disk()'
+        self.logic.create_new_disk()
     
     def winIDC_ok(self, event=None):
-        #print 'clsController.winIDC_ok()'
-        self.GUI.winIDC.destroy()
+        #print '.ClsController.winIDC_ok()'
+        self.gui.winIDC.destroy()
     
     def winIDC_cancel(self, event=None):
-        #print 'clsController.winIDC_cancel()'
-        self.GUI.winIDC.destroy()
+        #print '.ClsController.winIDC_cancel()'
+        self.gui.winIDC.destroy()
     
     def create_disk(self, event=None):
-        #print 'clsController.create_disk()'
-        self.Logic.create_disk()
+        #print '.ClsController.create_disk()'
+        self.logic.create_disk()
         
     def show_screen(self, event=None):
-        #print 'clsController.show_screen()'
-        self.Logic.show_screen()
+        #print '.ClsController.show_screen()'
+        self.logic.show_screen()
     
     def run(self, event=None):
-        self.GUI=self.root.GUI
-        self.Logic=self.root.Logic
-        self.CPU=self.root.CPU
-        #print 'clsController.run()'
-        self.Logic.run()
+        self.gui=self.root.gui
+        self.logic=self.root.logic
+        self.cpu=self.root.cpu
+        #print '.ClsController.run()'
+        self.logic.run()
     
     def about(self, event=None):
         #print 'about()'
-        self.root.GUI.winAbout.show()
+        self.root.gui.winAbout.show()
     
     def exit(self, event=None):
-        #print 'clsController.exit()'
-        self.root.Logic.exit()
+        #print '.ClsController.exit()'
+        self.root.logic.exit()
         
     def show_winLicense(self, event=None):
-        self.root.Logic.show_winLicense()
+        self.root.logic.show_winLicense()
         
     def hide_winLicense(self, event=None):
-        self.root.Logic.hide_winLicense()
+        self.root.logic.hide_winLicense()
     
     def show_winIDC(self, event=None):
-        self.root.Logic.show_winIDC()
+        self.root.logic.show_winIDC()
