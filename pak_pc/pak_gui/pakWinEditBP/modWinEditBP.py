eb18ec6a0bcd3f8b7627b5945bfcec16a225ac02
prospero78 <prospero.78.su@gmail.com>
2014-10-31 09:37:29 +0200

    0.1090 ADD: добавлены файлы по рекомендациям PyLint.

---
+++ pak_pc/pak_gui/pakWinEditBP/modWinEditBP.py
@@ -0,0 +1,85 @@
+# -*- coding: utf8 -*-
+'''
+Класс окна "О программе".
+'''
+
+from Tkinter import Toplevel, Frame, Button, Label, Entry, Checkbutton, IntVar
+
+class clsWinEditBP(Toplevel):
+    def __init__(self, root=None):
+        def create_self():
+            Toplevel.__init__(self)
+            self.state('withdrawn')
+            self.title(self.root.Res.winEditBP_title)
+            self.minsize(380, 200)
+        
+        def create_frmUp():
+            self.frmUp=Frame(self, border=3, relief='groove')
+            self.frmUp.pack(fill='both', expand=1, side='top')
+            
+            self.lblRegBP=Label(self.frmUp, border=3, relief='raised', text=' RegBP ', bg='white', fg='red', font='Arial 24 bold')
+            self.lblRegBP.pack(side='left', fill='y')
+            #--------------------------------------------------------------
+            self.frmAdrBreak=Frame(self.frmUp, border=3, relief='groove')
+            self.frmAdrBreak.pack(fill='x', side='top')
+            
+            self.lblAdrBreak=Label(self.frmAdrBreak, text='adr_break', relief='raised')
+            self.lblAdrBreak.pack(side='top', fill='x')
+            
+            self.entAdrBreakVal=Entry(self.frmAdrBreak, cursor='hand2')
+            self.entAdrBreakVal.pack(side='top', fill='x')
+            
+            self.entAdrBreakVal.insert(0, '0')
+            #--------------------------------------------------------------
+            self.frmAdrProc=Frame(self.frmUp, border=3, relief='groove')
+            self.frmAdrProc.pack(fill='x', side='top')
+            
+            self.lblAdrProc=Label(self.frmAdrProc, text='adr_proc', relief='raised')
+            self.lblAdrProc.pack(side='top', fill='x')
+            
+            self.entAdrProcVal=Entry(self.frmAdrProc, cursor='hand2')
+            self.entAdrProcVal.pack(side='top', fill='x')
+            
+            self.entAdrProcVal.insert(0, '0')
+            #--------------------------------------------------------------
+            self.frmAct=Frame(self.frmUp, border=3, relief='groove')
+            self.frmAct.pack(fill='both', expand=1, side='top')
+            
+            self.lblAct=Label(self.frmAct, text='act', relief='raised')
+            self.lblAct.pack(side='top', fill='x')
+            
+            self.Act=IntVar()
+            self.cheActVal=Checkbutton(self.frmAct, cursor='hand2', state='active', text='active register', variable=self.Act)
+            self.cheActVal.select()
+            self.cheActVal.pack(side='top', fill='x')
+            
+        
+        def create_frmBtn():
+            self.frmBtn=Frame(self, border=3, relief='raised')
+            self.frmBtn.pack(side='bottom', fill='x')
+            
+            self.btnClose=Button(self.frmBtn, text=self.root.Res.winEditBP_btnClose, bg='gray', command=self.destroy)
+            self.btnClose.pack(side='right')
+            
+        
+        self.root=root
+        create_self()
+        create_frmUp()
+        create_frmBtn()
+        
+        
+        
+    def show(self):
+        self.state('normal')
+        # показать поверх всех с фокусом
+        self.focus_set()
+        self.grab_set()
+        self.wait_window()
+        
+    def destroy(self):
+        self.state('withdrawn')
+        self.grab_release()
+        self.root.Control.winEditBP_hide()
+    
+    def win_exit(self):
+        self.destroy()
---
+++ pak_pc/pak_logic/modLogic.py
@@ -0,0 +1,219 @@
+# -*- coding: utf8 -*-
+'''
+Главный класс общей логики
+'''
+import sys
+class clsLogic:
+    def __init__(self, root=None):
+        self.root=root
+        # локальная переменная для регистра программного счётчика
+        self.RegPC_old=0
+        self.RegPC_val=0
+        # признаки запущенности ЦП в debug mode
+        self.debug=0
+    
+    def set_Res_str(self):
+        '''
+        Присваивает строковые ресурсы графическому интерфейсу.
+        Процедура сделана с целью отвязки GUI от ресурсов
+        (повышение атомарности класса).
+        '''
+        winMain=self.GUI.winMain
+        winMain.btnStep['text']=self.Res.winMain_btnStep
+        winMain.btnDebug['text']=self.Res.winMain_btnDebug_0
+        winMain.btnExit['text']=self.Res.winMain_btnExit_name
+        winMain.btnShowScreen['text']=self.Res.winMain_btnShowScreen_show
+        winMain.btnReset['text']=self.root.Res.winMain_btnReset
+        winMain.mbtFile['text']=self.Res.winMain_mbtFile_name
+        winMain.mbtEdit['text']=self.Res.winMain_mbtEdit_name
+        winMain.mbtCustom['text']=self.Res.winMain_mbtCustom_name
+        winMain.frmCPU.frmCpuFreq.lblKey['text']=self.Res.winMain_frmCpuFreq_lblKey
+        
+    def reset_pc(self):
+        '''
+        Сброс состояния виртуального компьютера.
+        '''
+        info={'com':'reset'}
+        self.CPU.qcom.put(info)
+        
+    def update_speed(self, dtime=0):
+        '''
+        При отладке обновляет периодически монитор состояния ЦП и скорость виртуальной машины.
+        '''
+        fr=1.0/dtime*self.CPU.time_code
+        #print dtime, self.CPU.frec
+        res=fr/self.CPU.frec
+        if res>1.1 or res<0.9:
+            if fr>self.CPU.frec:
+                self.CPU.frec=self.CPU.frec+int(fr/100)
+            elif fr<self.CPU.frec:
+                self.CPU.frec=self.CPU.frec-int(fr/100)
+            if fr>1000:
+                fr=str(int(self.CPU.frec/1000))+' kHz'
+            else:
+                fr=str(int(self.CPU.frec))+' Hz'
+            frec=self.GUI.winMain.frmCPU.frmCpuFrec
+            frec.entVal.delete(0,'end')
+            frec.entVal.insert(0, fr)
+        #self.GUI.winMain.update()
+        
+    def winEditBP_hide(self):
+        print '  clsLogic.winEditBP_hide()'
+        #------- обновить содержимое реального регистра программных прерываний --------
+        self.RegBP_act=self.GUI.winEditBP.Act.get()
+        self.RegBP_adr_break=int(self.GUI.winEditBP.entAdrBreakVal.get())
+        self.RegBP_adr_proc=int(self.GUI.winEditBP.entAdrProcVal.get())
+        
+        info={'RegBP':{'act':self.RegBP_act, 'adr_break':self.RegBP_adr_break, 'adr_proc':self.RegBP_adr_proc}}
+        self.CPU.qcom.put(info)
+        
+        #print 'act=', self.CPU.RegBP.get_act()
+        #self.update_monitor()
+        
+    def show_winEditBP(self):
+        #print 'clsLogic.show_winEditBP()'
+        self.GUI.winEditBP.show()
+       
+    def debug_CPU(self):
+        #print 'clsLogic.debug_CPU()'
+        if self.debug==0:
+            self.debug=1
+            self.GUI.winMain.btnDebug['text']=self.Res.winMain_btnDebug_1
+            info={'com':'debug(on)'}
+        else:
+            self.debug=0
+            self.GUI.winMain.btnDebug['text']=self.Res.winMain_btnDebug_0
+            info={'com':'debug(off)'}
+        self.CPU.qcom.put(info)
+       
+    def step_CPU(self):
+        '''
+        Метод исполняет шаг процессора с выводом результата.
+        Команды отправляются в очередь между процессами.
+        '''
+        #print 'clsLogic.step_CPU()'
+        #self.pre_update_monitor()
+        info={'com':'step()'}
+        self.CPU.qcom.put(info)
+        #self.post_update_monitor()
+        
+    def update_monitor(self):
+        while not self.CPU.qinfo.empty():
+            info=self.CPU.qinfo.get()
+            if info.has_key('RegA'):
+                inf=info['RegA']
+                #print 'detect RegA', inf
+                RegA=self.GUI.winMain.frmCPU.frmRegA
+                #print 'have key "RegA.val"!', info['RegA.val']
+                RegA.lblVal['text']=inf['val']
+                #print 'have key "RegA.FlagZ"!', info['RegA.FlagZ']
+                RegA.lblValZ['text']=inf['FlagZ']
+                #print 'have key "RegA.FlagO"!', info['RegA.FlagO']
+                RegA.lblValO['text']=inf['FlagO']
+                #print 'have key "RegA.FlagC"!', info['RegA.FlagC']
+                RegA.lblValC['text']=inf['FlagO']
+            if info.has_key('RegPC'):
+                inf=info['RegPC']
+                #print 'detect RegPC', inf
+                #print 'have key "RegPC.val"!', info['RegPC.val']
+                self.RegPC_val=inf['val']
+                self.GUI.winMain.frmCPU.frmRegPC.lblVal['text']=self.RegPC_old
+                self.RegPC_old=self.RegPC_val
+            #---------------------------
+            if info.has_key('RegBP'):
+                inf=info['RegBP']
+                RegBP=self.GUI.winMain.frmCPU.frmRegBP
+                #print 'have key "RegBP.act"!', info['RegBP.act']
+                RegBP.lblActVal['text']=inf['act']
+                #print 'have key "RegBP.adr_proc"!', info['RegBP.adr_proc']
+                RegBP.lblProcVal['text']=inf['adr_proc']
+                #print 'have key "RegBP.adr_break"!', info['RegBP.adr_break']
+                RegBP.lblBreakVal['text']=inf['adr_break']
+            #---------------------------
+            if info.has_key('RegSP'):
+                inf=info['RegSP']
+                RegSP=self.GUI.winMain.frmCPU.frmRegSP
+                RegSP.lblAdrVal['text']=inf['adr']
+                RegSP.lblValVal['text']=inf['val']
+            #---------------------------
+            if info.has_key('debug'):
+                inf=info['debug']
+                #print 'detect DEBUG', inf
+            #---------------------------
+            if info.has_key('dtime'):
+                inf=info['dtime']
+                #print 'detect DTIME', inf
+                self.update_speed(dtime=inf)
+        while not self.Video.vout.empty():
+            vout=self.Video.vout.get()
+            self.winScreen.lblScreen['text']=vout
+    def generate_new_disk(self):
+        #print 'generate_new_disk()'
+        self.GUI.winCreateDisk.destroy()
+        disk_size=int(self.GUI.winCreateDisk.fkvSize.get_val())
+        disk_name=self.GUI.winCreateDisk.fkvName.get_val()
+        str_='\0'*(2**10)
+        f=open('./data/'+disk_name+'.dsk','wb')
+        for i in xrange(0, disk_size):
+            f.write(str_)
+        f.close()
+        
+    def create_new_disk(self):
+        #TODO: дописать процедуру создания нового диска
+        pass
+    
+    def create_disk(self):
+        print 'clsLogic.create_disk()'
+        self.GUI.winCreateDisk.show()
+        
+    def show_screen(self):
+        if not self.GUI.winScreen.winScreen_show:
+            self.GUI.winScreen.show()
+        else:
+            self.GUI.winScreen.destroy()
+    def run(self):
+        self.CPU=self.root.CPU
+        self.GUI=self.root.GUI
+        self.winScreen=self.GUI.winScreen
+        self.Res=self.root.Res
+        self.Video=self.root.Video
+        self.Video.start()
+        self.CPU.start()
+        
+        info={'com':'get_info()'}
+        self.CPU.qcom.put(info)
+        
+        # присвоение строковых ресурсов
+        self.set_Res_str()
+        
+        self.GUI.run()
+        
+    def exit(self):
+        '''
+        Общесистемный выход из программы.
+        Всякие финальные действия.
+        '''
+        #print 'clsLogic.exit()'
+        try:
+            self.root.GUI.winEditBP.win_exit()
+            self.root.GUI.winScreen.win_exit()
+            self.root.GUI.winLicense.win_exit()
+            self.root.GUI.winIDC.win_exit()
+            self.root.GUI.winCreateDisk.win_exit()
+            self.root.GUI.winAbout.win_exit()
+            self.root.GUI.winMain.win_exit()
+            self.root.CPU.terminate()
+            self.root.Video.terminate()
+            del self.root.CPU
+        finally:
+            sys.exit(0)
+        
+    def show_winLicense(self):
+        self.root.GUI.winLicense.show()
+    
+    def show_winIDC(self):
+        self.root.GUI.winIDC.show()
+        
+    def hide_winLicense(self):
+        if 'lin' not in sys.platform:
+            self.root.GUI.winAbout.grab_set()
