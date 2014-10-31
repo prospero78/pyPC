fa3a47f9a0d3363cea00877901cbc4dcb870aa2a
prospero78 <prospero.78.su@gmail.com>
2014-10-31 14:46:50 +0200

    0.1175 FIX: исправление имён ресурсов (рекомендация PyLint).

--- pak_pc/pak_gui/pak_win_idc/mod_win_idc.py
+++ pak_pc/pak_gui/pak_win_idc/mod_win_idc.py
@@ -5,39 +5,40 @@
 
 from Tkinter import Toplevel, Frame, Button, Label
 
-from pak_pc.pak_gui.pak_widgets.modFrmDiskSelect import clsFrmDiskSelect
+from pak_pc.pak_gui.pak_widgets.mod_frm_disk_select import clsFrmDiskSelect
 
-class clsWinIDC(Toplevel):
+class ClsWinIDC(Toplevel):
     def __init__(self, root=None):
         def create_self():
             Toplevel.__init__(self)
             self.state('withdrawn')
-            self.title(self.root.res.winIDC_name)
+            self.title(self.lang['win_idc_name'])
             self.minsize(350, 200)
         
         def create_frmBtn():
-            self.frmBtn=Frame(self, border=2, relief='sunken')
-            self.frmBtn.pack(side='bottom', fill='x')
+            self.frm_btn=Frame(self, border=2, relief='sunken')
+            self.frm_btn.pack(side='bottom', fill='x')
             
-            self.btnCancel=Button(self.frmBtn, text=self.root.res.winIDC_cancel, command=self.root.control.winIDC_cancel)
+            self.btnCancel=Button(self.frm_btn, text=self.lang['win_idc_cancel'], command=self.root.control.winIDC_cancel)
             self.btnCancel.pack(side='right')
             
-            self.btnReset=Button(self.frmBtn, text=self.root.res.winIDC_reset)
+            self.btnReset=Button(self.frm_btn, text=self.lang['win_idc_reset'])
             self.btnReset.pack(side='right')
             
-            self.btnOk=Button(self.frmBtn, text=' Ok ', command=self.root.control.winIDC_ok)
-            self.btnOk.pack(side='right')
+            self.btn_ok=Button(self.frm_btn, text=' Ok ', command=self.lang['win_idc_ok'])
+            self.btn_ok.pack(side='right')
             
         def create_frmIDC():
-            self.frmIDC=Frame(self, border=2, relief='sunken')
-            self.frmIDC.pack(side='top', fill='both', expand=1)
+            self.frm_idc=Frame(self, border=2, relief='sunken')
+            self.frm_idc.pack(side='top', fill='both', expand=1)
             
-            self.frmDisk0=clsFrmDiskSelect(master=self.frmIDC, root=self.root, text='Disk-0:')
-            self.frmDisk1=clsFrmDiskSelect(master=self.frmIDC, root=self.root, text='Disk-1:', path='None')
-            self.frmDisk2=clsFrmDiskSelect(master=self.frmIDC, root=self.root, text='Disk-2:', path='None')
-            self.frmDisk3=clsFrmDiskSelect(master=self.frmIDC, root=self.root, text='Disk-3:', path='None')
+            self.frmDisk0=clsFrmDiskSelect(master=self.frm_idc, root=self.root, text='Disk-0:')
+            self.frmDisk1=clsFrmDiskSelect(master=self.frm_idc, root=self.root, text='Disk-1:', path='None')
+            self.frmDisk2=clsFrmDiskSelect(master=self.frm_idc, root=self.root, text='Disk-2:', path='None')
+            self.frmDisk3=clsFrmDiskSelect(master=self.frm_idc, root=self.root, text='Disk-3:', path='None')
             
         self.root=root
+        self.lang=root.res.lang_str.lang_dict
         create_self()
         create_frmBtn()
         create_frmIDC()
