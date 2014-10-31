578a16fccb644b412491b9eafce264b483137948
prospero78 <prospero.78.su@gmail.com>
2014-10-31 16:20:57 +0200

    0.1218 Многичсленные исправления имён в классах (рекомендации PyLint).

--- pak_pc/pak_gui/pak_widgets/mod_frm_reg_bp.py
+++ pak_pc/pak_gui/pak_widgets/mod_frm_reg_bp.py
@@ -12,8 +12,8 @@ class ClsFrmRegPC(Frame):
         Frame.__init__(self, master=root, border=2, relief='ridge')
         self.pack(fill='x', side='top')
         
-        self.lblName=Label(self, text='reg_pc', border=1, relief='ridge')
-        self.lblName.pack(side='left', fill='x')
+        self.lbl_name=Label(self, text='reg_pc', border=1, relief='ridge')
+        self.lbl_name.pack(side='left', fill='x')
         
         self.lblBreak=Label(self, text='adr_break', border=1, relief='groove', bg='gray')
         self.lblBreak.pack(side='left')
@@ -27,11 +27,11 @@ class ClsFrmRegPC(Frame):
         self.lblProcVal=Label(self, text='0', border=1, relief='groove')
         self.lblProcVal.pack(side='left')
         
-        self.lblAct=Label(self, text='act', border=1, relief='groove', bg='gray')
-        self.lblAct.pack(side='left')
+        self.lbl_act=Label(self, text='act', border=1, relief='groove', bg='gray')
+        self.lbl_act.pack(side='left')
         
-        self.lblActVal=Label(self, text='0', border=1, relief='groove')
-        self.lblActVal.pack(side='left')
+        self.lbl_act_val=Label(self, text='0', border=1, relief='groove')
+        self.lbl_act_val.pack(side='left')
         
 if __name__=='__main__':
     from Tkinter import Tk
@@ -39,5 +39,5 @@ if __name__=='__main__':
     root.title('test frmreg_pc')
     root.minsize(300,200)
     reg=ClsFrmRegPC(root=root)
-    reg.lblName['text']='reg_pc'
+    reg.lbl_name['text']='reg_pc'
     root.mainloop()
