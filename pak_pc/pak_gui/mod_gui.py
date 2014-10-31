f6c2f34ace2e8035f9f2476faa8f4dfe3c80b4e6
prospero78 <prospero.78.su@gmail.com>
2014-10-31 13:11:09 +0200

    0.1147 AUTOFIX: добавление новых файлов (рекомендация по именам модулей PyLint).

---
+++ pak_pc/pak_gui/mod_gui.py
@@ -0,0 +1,44 @@
+# -*- coding: utf8 -*-
+'''
+Инициализация класса графики.
+'''
+
+class clsGUI:
+    def __init__(self, root=None):
+        self.root=root
+        # импорт главного класса окна
+        from pak_win_main.mod_win_main import clsWinMain
+        self.winMain=clsWinMain(root=self.root)
+        
+        # импорт окна "О программе"
+        from pak_win_about.modWinAbout import clsWinAbout
+        self.winAbout=clsWinAbout(root=self.root)
+        
+        # импорт окна лицензии
+        from pak_win_license.mod_win_license import clsWinLicense
+        self.winLicense=clsWinLicense(root=self.root)
+        
+        # импорт окна экрана виртуального компьютера
+        from pakWinScreen.modWinScreen import clsWinScreen
+        self.winScreen=clsWinScreen(root=self.root)
+        
+        # импорт окна для создания нового диска
+        from pakWinCreateDisk.modWinCreateDisk import clsWinCreateDisk
+        self.winCreateDisk=clsWinCreateDisk(root=self.root)
+        
+        # импорт окна создания/монтирования дисков (IDC)
+        from pakWinIDC.modWinIDC import clsWinIDC
+        self.winIDC=clsWinIDC(root=self.root)
+        
+        # импорт окна редактирования настроек регистра программного прерывания (winBP)
+        from pakWinEditBP.modWinEditBP import clsWinEditBP
+        self.winEditBP=clsWinEditBP(root=self.root)
+        
+        
+    def run(self):
+        '''
+        Класс загружает графику, должен запускаться последним!!!
+        '''
+        # запуск бесконечной петли
+        self.winMain.begin()
+        pass
