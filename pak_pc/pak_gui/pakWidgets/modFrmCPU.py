4c9681a5d257e41f8f83ae2b10ab354792a2f45a
prospero78 <prospero.78.su@gmail.com>
2014-10-31 10:11:27 +0200

    0.1119 AUTOFIX: исправление путей импорта.

--- pak_pc/pak_gui/pakWidgets/modFrmCPU.py
+++ pak_pc/pak_gui/pakWidgets/modFrmCPU.py
@@ -6,22 +6,22 @@
 
 from Tkinter import Frame
 
-from pakPC.pak_gui.pakWidgets.modFrmReg import clsFrmReg
-from pakPC.pak_gui.pakWidgets.modFrmreg_pc import clsFrmreg_pc
-from pakPC.pak_gui.pakWidgets.modFrmreg_pc import clsFrmreg_pc
-from pakPC.pak_gui.pakWidgets.modFrmCpuFrec import clsFrmCpuFreq
-from pakPC.pak_gui.pakWidgets.modFrmRegSP import clsFrmRegSP
+from pakPC.pak_gui.pak_widgets.mod_frm_reg import clsFrmReg
+from pakPC.pak_gui.pak_widgets.mod_frm_reg_pc import ClsFrmRegPC
+from pakPC.pak_gui.pak_widgets.mod_frm_reg_sp import ClsFrmRegSP
+from pakPC.pak_gui.pak_widgets.mod_frm_cpu_frec import clsFrmCpuFreq
+from pakPC.pak_gui.pak_widgets.modFrmRegSP import clsFrmRegSP
 
-class clsFrmCPU(Frame):
+class ClsFrmCPU(Frame):
     def __init__(self, root=None):
         Frame.__init__(self, master=root, border=3, relief='sunken')
         self.pack(side='top', fill='x')
         
         # отображаем регистр программной отладки
-        self.frmreg_pc=clsFrmreg_pc(root=self)
+        self.frmreg_pc=ClsFrmRegPC(root=self)
         
         # отображаем программный счётчик
-        self.frmreg_pc=clsFrmreg_pc(root=self)
+        self.frmreg_pc=ClsFrmRegPC(root=self)
         #self.frmRegA.lblName['text']='RegA'
         
         # отображаем указатель стека
