1e059243abbe810653aa2712a7ccc1dcfbc8c4c5
prospero78 <prospero.78.su@gmail.com>
2014-10-31 09:55:57 +0200

    0.1101 AUTOFIX: автоисправление путей импорта в проекте PytCharm.

--- pak_pc/pak_gui/pakWidgets/modFrmRegPC.py
+++ pak_pc/pak_gui/pakWidgets/modFrmRegPC.py
@@ -6,13 +6,13 @@
 
 from Tkinter import Frame, Label
 
-class clsFrmRegPC(Frame):
+class clsFrmreg_pc(Frame):
     def __init__(self, root=None):
         self.root=root
         Frame.__init__(self, master=root, border=2, relief='ridge')
         self.pack(fill='x', side='top')
         
-        self.lblName=Label(self, text='RegPC', border=1, relief='ridge')
+        self.lblName=Label(self, text='reg_pc', border=1, relief='ridge')
         self.lblName.pack(side='left', fill='x')
         
         self.lblVal=Label(self, text='0000', border=1, relief='groove')
@@ -22,8 +22,8 @@ class clsFrmRegPC(Frame):
 if __name__=='__main__':
     from Tkinter import Tk
     root=Tk()
-    root.title('test frmRegPC')
+    root.title('test frmreg_pc')
     root.minsize(300,200)
-    reg=clsFrmRegPC(root=root)
-    reg.lblName['text']='RegPC'
+    reg=clsFrmreg_pc(root=root)
+    reg.lblName['text']='reg_pc'
     root.mainloop()
