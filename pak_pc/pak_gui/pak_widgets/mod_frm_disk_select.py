be6ae021d78e7993764818f19bf1b07c66aa5a72
prospero78 <prospero.78.su@gmail.com>
2014-10-31 15:06:06 +0200

    0.1190 FIX: исправление имён импорта и методов (рекомендация PyLint).

--- pak_pc/pak_gui/pak_widgets/mod_frm_disk_select.py
+++ pak_pc/pak_gui/pak_widgets/mod_frm_disk_select.py
@@ -6,7 +6,7 @@
 from Tkinter import Frame, Label, Button
 from tkFileDialog import askopenfilename
 
-class clsFrmDiskSelect(Frame):
+class ClsFrmDiskSelect(Frame):
     def __init__(self,
                 master=None,
                 root=None,
@@ -18,8 +18,8 @@ class clsFrmDiskSelect(Frame):
                         initialfile='./data/default.dsk',
                         filetypes=[('Disk images','.dsk'),],
                         title=self.root.res.winIDC_OpenDiskImage)
-                self.root.GUI.winIDC.lift()
-                self.root.GUI.winIDC.focus_get()
+                self.root.gui.winIDC.lift()
+                self.root.gui.winIDC.focus_get()
             Frame.__init__(self, master=self.master)
             self.pack(side='top', fill='x')
             
@@ -29,17 +29,18 @@ class clsFrmDiskSelect(Frame):
             self.lblDiskPath=Label(self, text=self.path, anchor='w', border=2, relief='groove')
             self.lblDiskPath.pack(side='left', fill='both', expand=1)
             
-            self.btnCreate=Button(self, text=self.root.res.winIDC_ImageCreate, command=self.root.control.create_disk)
+            self.btnCreate=Button(self, text=self.lang['win_idc_image_create'], command=self.root.control.create_disk)
             self.btnCreate.pack(side='left')
             
-            self.btnOpen=Button(self, text=self.root.res.winIDC_open, command=open_disk)
+            self.btnOpen=Button(self, text=self.lang['win_idc_open'], command=open_disk)
             self.btnOpen.pack(side='left')
             
-            self.btnClear=Button(self, text=self.root.res.winIDC_ImageUnpath)
+            self.btnClear=Button(self, text=self.lang['win_idc_image_unpath'])
             self.btnClear.pack(side='left')
             
             
         self.root=root
+        self.lang=root.res.lang_str.lang_dict
         self.master=master
         self.path=path
         self.text=text
