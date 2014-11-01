1e519acb5b363aa24c9ff4ac66803710fab73d97
prospero78 <prospero.78.su@gmail.com>
2014-11-01 16:12:49 +0200

    0.1248 FIX: исправлено форматирование кода. Рейтинг PyLint = 8.62/10

--- pak_pc/pak_gui/pak_widgets/mod_frm_reg_bp.py
+++ pak_pc/pak_gui/pak_widgets/mod_frm_reg_bp.py
@@ -1,43 +1,64 @@
 # -*- coding: utf8 -*-
-'''
+"""
 Модуль отображения регистра программного прерывания.
 Будет показывать содержимое и его флаг активности.
-'''
+"""
 
 from Tkinter import Frame, Label
 
+
 class ClsFrmRegBP(Frame):
+    """
+    Фрейм выводит состояние регистра программного прерывания.
+    :param root:
+    """
+
     def __init__(self, root=None):
-        self.root=root
+        """
+        Создаёт экземпляр класса.
+        :param root: ссылка на корневой класс приложения.
+        """
+        self.root = root
         Frame.__init__(self, master=root, border=2, relief='ridge')
         self.pack(fill='x', side='top')
-        
-        self.lbl_name=Label(self, text='reg_pc', border=1, relief='ridge')
+
+        self.lbl_name = Label(self, text='reg_pc', border=1, relief='ridge')
         self.lbl_name.pack(side='left', fill='x')
-        
-        self.lblBreak=Label(self, text='adr_break', border=1, relief='groove', bg='gray')
-        self.lblBreak.pack(side='left')
-        
-        self.lblBreakVal=Label(self, text='0', border=1, relief='groove')
-        self.lblBreakVal.pack(side='left')
-        
-        self.lblProc=Label(self, text='adr_proc', border=1, relief='groove', bg='gray')
-        self.lblProc.pack(side='left')
-        
-        self.lblProcVal=Label(self, text='0', border=1, relief='groove')
-        self.lblProcVal.pack(side='left')
-        
-        self.lbl_act=Label(self, text='act', border=1, relief='groove', bg='gray')
+
+        self.lbl_break = Label(self,
+                               text='adr_break',
+                               border=1,
+                               relief='groove',
+                               bg='gray')
+        self.lbl_break.pack(side='left')
+
+        self.lbl_break_val = Label(self, text='0', border=1, relief='groove')
+        self.lbl_break_val.pack(side='left')
+
+        self.lbl_proc = Label(self,
+                              text='adr_proc',
+                              border=1,
+                              relief='groove',
+                              bg='gray')
+        self.lbl_proc.pack(side='left')
+
+        self.lbl_proc_val = Label(self, text='0', border=1, relief='groove')
+        self.lbl_proc_val.pack(side='left')
+
+        self.lbl_act = Label(self, text='act', border=1, relief='groove',
+                             bg='gray')
         self.lbl_act.pack(side='left')
-        
-        self.lbl_act_val=Label(self, text='0', border=1, relief='groove')
+
+        self.lbl_act_val = Label(self, text='0', border=1, relief='groove')
         self.lbl_act_val.pack(side='left')
-        
-if __name__=='__main__':
+
+
+if __name__ == '__main__':
     from Tkinter import Tk
-    root=Tk()
-    root.title('test frmreg_pc')
-    root.minsize(300,200)
-    reg=ClsFrmRegPC(root=root)
-    reg.lbl_name['text']='reg_pc'
-    root.mainloop()
+
+    win_test = Tk()
+    win_test.title('test frmreg_pc')
+    win_test.minsize(300, 200)
+    reg = ClsFrmRegBP(root=win_test)
+    reg.lbl_name['text'] = 'reg_pc'
+    win_test.mainloop()
