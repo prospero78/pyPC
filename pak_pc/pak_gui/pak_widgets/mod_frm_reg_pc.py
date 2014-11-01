9fb71b351042a6a3d6739e019c179dec419ee6c1
prospero78 <prospero.78.su@gmail.com>
2014-11-01 16:16:17 +0200

    0.1249 FIX: исправлено форматирование кода. Рейтинг PyLint = 8.42/10

--- pak_pc/pak_gui/pak_widgets/mod_frm_reg_pc.py
+++ pak_pc/pak_gui/pak_widgets/mod_frm_reg_pc.py
@@ -1,29 +1,40 @@
 # -*- coding: utf8 -*-
-'''
+"""
 Модуль отображения регистра программного счётчика.
 Будет показывать содержимое и флаги.
-'''
+"""
 
 from Tkinter import Frame, Label
 
+
 class ClsFrmRegPC(Frame):
+    """
+    Обеспечивает отображение состояия регистра программного счётчика.
+    :param root: ссылка на корневой класс.
+    """
+
     def __init__(self, root=None):
-        self.root=root
+        """
+        Создаёт фрейм отображения состояния регистра программного счётчика.
+        :param root: ссылка на корневой элемент.
+        """
+        self.root = root
         Frame.__init__(self, master=root, border=2, relief='ridge')
         self.pack(fill='x', side='top')
-        
-        self.lbl_name=Label(self, text='reg_pc', border=1, relief='ridge')
+
+        self.lbl_name = Label(self, text='reg_pc', border=1, relief='ridge')
         self.lbl_name.pack(side='left', fill='x')
-        
-        self.lbl_val=Label(self, text='0000', border=1, relief='groove')
+
+        self.lbl_val = Label(self, text='0000', border=1, relief='groove')
         self.lbl_val.pack(side='left')
 
-        
-if __name__=='__main__':
+
+if __name__ == '__main__':
     from Tkinter import Tk
-    root=Tk()
-    root.title('test frmreg_pc')
-    root.minsize(300,200)
-    reg=ClsFrmRegPC(root=root)
-    reg.lbl_name['text']='reg_pc'
-    root.mainloop()
+
+    win_test = Tk()
+    win_test.title('test frmreg_pc')
+    win_test.minsize(300, 200)
+    reg = ClsFrmRegPC(root=win_test)
+    reg.lbl_name['text'] = 'reg_pc'
+    win_test.mainloop()
