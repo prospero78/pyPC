f760a1492321feceae907d92827d782ca031161a
prospero78 <prospero.78.su@gmail.com>
2014-11-01 15:15:55 +0200

    0.1247 FIX: исправлено форматирование кода. Рейтинг PyLint = 9.41/10

--- pak_pc/pak_gui/pak_widgets/mod_frm_cpu.py
+++ pak_pc/pak_gui/pak_widgets/mod_frm_cpu.py
@@ -12,24 +12,31 @@ from pak_pc.pak_gui.pak_widgets.mod_frm_reg_bp import ClsFrmRegBP
 from pak_pc.pak_gui.pak_widgets.mod_frm_cpu_frec import ClsFrmCpuFreq
 from pak_pc.pak_gui.pak_widgets.mod_frm_reg_sp import ClsFrmRegSP
 
+
 class ClsFrmCPU(Frame):
+    """
+    Класс описывающий фрейм для отображения информации о состоянии
+    процессора.
+    :param root: ссылка на корневой класс.
+    """
+
     def __init__(self, root=None):
         Frame.__init__(self, master=root, border=3, relief='sunken')
         self.pack(side='top', fill='x')
-        
+
         # отображаем регистр программной отладки
-        self.frm_reg_bp=ClsFrmRegBP(root=self)
-        
+        self.frm_reg_bp = ClsFrmRegBP(root=self)
+
         # отображаем программный счётчик
-        self.frm_reg_pc=ClsFrmRegPC(root=self)
-        #self.frm_reg_a.lbl_name['text']='reg_a'
-        
+        self.frm_reg_pc = ClsFrmRegPC(root=self)
+        # self.frm_reg_a.lbl_name['text']='reg_a'
+
         # отображаем указатель стека
-        self.frm_reg_sp=ClsFrmRegSP(root=self)
-        
+        self.frm_reg_sp = ClsFrmRegSP(root=self)
+
         # отображение регистра А
-        self.frm_reg_a=ClsFrmReg(root=self)
-        self.frm_reg_a.lbl_name['text']='reg_a'
-        
+        self.frm_reg_a = ClsFrmReg(root=self)
+        self.frm_reg_a.lbl_name['text'] = 'reg_a'
+
         # отображение частоты виртуального процессора
-        self.frm_cpu_freq=ClsFrmCpuFreq(root=self)
+        self.frm_cpu_freq = ClsFrmCpuFreq(root=self)
