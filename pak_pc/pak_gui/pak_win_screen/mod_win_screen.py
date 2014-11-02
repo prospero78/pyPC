f454af68e327de3f889ec5068a520aed86773295
prospero78 <prospero.78.su@gmail.com>
2014-11-02 10:05:15 +0200

    0.1274 FIX: исправлено форматирование кода. Рейтинг PyLint = 8.14/10

--- pak_pc/pak_gui/pak_win_screen/mod_win_screen.py
+++ pak_pc/pak_gui/pak_win_screen/mod_win_screen.py
@@ -3,64 +3,97 @@
 Класс окна экрана виртуального компьютера.
 '''
 
-from Tkinter import Toplevel, Frame, Button, Canvas, Label
+from Tkinter import Toplevel, Frame, Button, Label
+
 
 class ClsWinScreen(Toplevel):
+    """
+    Показывает экран виртуального компьютера.
+    """
     def __init__(self, root=None):
+        """
+        Создаёт отдельное окно под экран виртуального компьютера.
+        :param root: ссылка на корневой объект
+        :return:
+        """
         def create_self():
+            """
+            Создание самого окна.
+            :return:
+            """
             Toplevel.__init__(self)
             self.state('withdrawn')
             self.title(self.lang['win_screen_title'])
             self.minsize(640, 480)
-        
-        def create_frmScreen():
-            self.frmScreen=Frame(self, border=3, relief='groove')
-            self.frmScreen.pack(fill='both', expand=1, side='top')
-            
-            #self.cnvScreen=Canvas(self.frmScreen, bg='white', width=640, height=480)
+
+        def create_frm_screen():
+            """
+            Создание формы для отображения окна вывода.
+            :return:
+            """
+            self.frm_screen = Frame(self, border=3, relief='groove')
+            self.frm_screen.pack(fill='both', expand=1, side='top')
+
+            # self.cnvScreen=Canvas(self.frm_screen,
+            #                       bg='white', width=640, height=480)
             #self.cnvScreen.pack(fill='both')
-            
-            self.lblScreen=Label(self.frmScreen, bg='black', fg='yellow', font='Consolas 10')
-            self.lblScreen.pack(fill='both')
-            
-            a=''
-            for i in xrange(0,40):
-                for i1 in xrange(0,80):
-                    a+=' '
-                a+='\n'
-            self.lblScreen['text']=a
-        
-        def create_frmBtn():
-            self.frm_btn=Frame(self, border=3, relief='raised')
+
+            self.lbl_screen = Label(self.frm_screen, bg='black', fg='yellow',
+                                    font='Consolas 10')
+            self.lbl_screen.pack(fill='both')
+
+            str_out = ''
+            for row in xrange(0, 40):
+                for col in xrange(0, 80):
+                    str_out += ' '
+                    str_out += '\n'
+            self.lbl_screen['text'] = str_out
+
+        def create_frm_btn():
+            """
+            Создание фрейма с кнопками.
+            :return:
+            """
+            self.frm_btn = Frame(self, border=3, relief='raised')
             self.frm_btn.pack(side='bottom', fill='x')
-            
-            self.btnScreenClose=Button(self.frm_btn, text=self.lang['win_screen_btn_close_text'], bg='gray', command=self.destroy)
-            self.btnScreenClose.pack(side='right')
-            
-        self.root=root
-        self.lang=root.res.lang_str.lang_dict
+
+            self.btn_screen_close = Button(self.frm_btn, text=self.lang[
+                'win_screen_btn_close_text'], bg='gray', command=self.destroy)
+            self.btn_screen_close.pack(side='right')
+
+        self.root = root
+        self.lang = root.res.lang_str.lang_dict
         create_self()
-        create_frmBtn()
-        create_frmScreen()
+        create_frm_btn()
+        create_frm_screen()
         # признак отображённости окна терминала
-        self.winScreen_show=0
-        
-        
-        
+        self.win_screen_show = 0
+
+
     def show(self):
-        self.winScreen_show=1
+        """
+        Показывает окно экрана виртуального компьютера.
+        :return:
+        """
+        self.win_screen_show = 1
         self.state('normal')
         # показать поверх всех с фокусом без удержания фокуса
         self.focus_set()
-        #self.grab_set()
+        # self.grab_set()
         #self.wait_window()
-        self.root.gui.win_main.btnShowScreen['text']=self.root.res.winMain_btnShowScreen_hide
-        
+        self.root.gui.win_main.btn_show_screen[
+            'text'] = self.root.res.winMain_btnShowScreen_hide
+
     def destroy(self):
-        self.winScreen_show=0
+        """
+        Скрывает окно экрана виртуального компьютера.
+        :return:
+        """
+        self.win_screen_show = 0
         self.state('withdrawn')
         self.grab_release()
-        self.root.gui.win_main.btnShowScreen['text']=self.lang['win_main_btn_show_screen_show']
-    
+        self.root.gui.win_main.btn_show_screen['text'] = self.lang[
+            'win_main_btn_show_screen_show']
+
     def win_exit(self):
         self.destroy()
