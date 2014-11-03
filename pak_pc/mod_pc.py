0ee12d79e302e4d16a1f5847ac079fca0b04ce61
prospero78 <prospero.78.su@gmail.com>
2014-11-03 17:30:39 +0200

    0.1279 FIX: исправлено форматирование кода. Рейтинг PyLint = 9.50/10

--- pak_pc/mod_pc.py
+++ pak_pc/mod_pc.py
@@ -1,45 +1,58 @@
 # -*- coding: utf8 -*-
-'''
+"""
 Главный класс для работы pyPC.
-'''
+"""
+
 
 class ClsPC(object):
-    '''
+    """
     Класс реализует логику работы всей системы.
-    '''
+    """
+
     def __init__(self, arg=None):
         # импорт класса ресурсов
+        """
+        Создание главного класса-приложения.
+        :param arg:
+        """
         from pak_pc.pak_resurs.mod_resurs import ClsRes
+
         self.res = ClsRes(root=self, lang='ru', arg=arg)
 
         # импорт класса видеокарты
         from pak_pc.pak_cpu.pak_video.mod_video import ClsVideo
+
         self.video = ClsVideo()
 
         # импорт клаcса центрального процессора
         from pak_pc.pak_cpu.mod_cpu import ClsCPU
+
         self.cpu = ClsCPU(vcom=self.video.vcom,
                           vinfo=self.video.vinfo)
 
         # импорт класса общей логики
         from pak_pc.pak_logic.mod_logic import ClsLogic
+
         self.logic = ClsLogic(root=self)
 
         # импорт класса контроллера
         from pak_pc.pak_controller.mod_controller import ClsController
+
         self.control = ClsController(root=self)
 
         # импорт класса графики
         from pak_pc.pak_gui.mod_gui import ClsGUI
+
         self.gui = ClsGUI(root=self)
 
         # импорт класса интерфейса дискового кластера
         from pak_pc.pak_idc.mod_idc import ClsIDC
+
         self.idc = ClsIDC(root=self)
 
     def run(self):
-        '''
+        """
         Запускает главный цикл изменений.
-        '''
+        """
         self.control.run()
         return 0
