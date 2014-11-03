923b3b88d14ba3601c9d9d9d74bdf4b9d249160c
prospero78 <prospero.78.su@gmail.com>
2014-11-03 17:44:48 +0200

    0.1281 FIX: исправлено форматирование кода. Рейтинг PyLint = 10.0/10!!! )))

--- pak_pc/pak_cpu/pak_mem/mod_memory.py
+++ pak_pc/pak_cpu/pak_mem/mod_memory.py
@@ -1,30 +1,59 @@
 # -*- coding: utf8 -*-
-'''
+"""
 Класс памяти. Будем со временем дорабатывать.
-'''
+"""
 
-class ClsMemory:
-    def __init__(self, max_adr=2**22):
+
+class ClsMemory(object):
+    """
+    Описывает память в виде болчного устройства.
+    """
+    def __init__(self, max_adr=2 ** 22):
+        """
+        Начальная инициализация памяти.
+        :param max_adr:
+        :return:
+        """
         # максимально допустимое количество памяти
-        self.max_adr=max_adr
+        self.max_adr = max_adr
         # текущее количество памяти
-        self.act_mem=1024
+        self.act_mem = 1024
         # инициализация памяти виртуального компьютера
-        self.adr={}
+        self.adr = {}
         for i in xrange(0, self.act_mem):
-            self.adr[i]=0
-    
+            self.adr[i] = 0
+
     def get_adr(self, adr):
+        """
+        Возвращает значение ячейки памяти по адресу (для cython)
+        :param adr:
+        :return:
+        """
         return self.adr[adr]
-        
+
     def set_adr(self, adr, val):
-        self.adr[adr]=val
-        
+        """
+        Устанавливает ячейку памяти в значение. (для cython)
+        :param adr:
+        :param val:
+        :return:
+        """
+        self.adr[adr] = val
+
     def get_max_adr(self):
+        """
+        Возвращает максимально-доступный адрес памяти.
+        Выше его быть памяти в любом случае не может.
+        :return:
+        """
         return self.max_adr
-        
+
     def add_memory(self):
-        if self.act_mem<self.max_adr-1024:
-            for i in xrange(self.act_mem, self.act_mem+1024):
-                self.adr[i]=0
-            self.act_mem+=1024
+        """
+        Добавляет памяти блоками по 1024 ячейки (НЕ БАЙТА!!!)
+        :return:
+        """
+        if self.act_mem < self.max_adr - 1024:
+            for i in xrange(self.act_mem, self.act_mem + 1024):
+                self.adr[i] = 0
+            self.act_mem += 1024
