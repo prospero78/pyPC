06c8859c5930658efd3606f39118b27024722de2
prospero78 <prospero.78.su@gmail.com>
2014-11-03 18:28:20 +0200

    0.1288 FIX: исправлено форматирование кода. Рейтинг PyLint = 6.34/10.

--- pak_pc/pak_gui/pak_win_idc/mod_win_idc.py
+++ pak_pc/pak_gui/pak_win_idc/mod_win_idc.py
@@ -1,58 +1,103 @@
 # -*- coding: utf8 -*-
-'''
+"""
 Класс окна интерфейса дискового кластера.
-'''
+"""
 
-from Tkinter import Toplevel, Frame, Button, Label
+from Tkinter import Toplevel, Frame, Button
 
 from pak_pc.pak_gui.pak_widgets.mod_frm_disk_select import ClsFrmDiskSelect
 
+
 class ClsWinIDC(Toplevel):
+    """
+    Окно интерфейса дискового кластера.
+    """
     def __init__(self, root=None):
+        """
+        Создаёт окно интерфейса дискового кластера.
+        :param root:
+        :return:
+        """
         def create_self():
+            """
+            Создание окна самого себя.
+            :return:
+            """
             Toplevel.__init__(self)
             self.state('withdrawn')
             self.title(self.lang['win_idc_name'])
             self.minsize(350, 200)
-        
-        def create_frmBtn():
-            self.frm_btn=Frame(self, border=2, relief='sunken')
+
+        def create_frm_btn():
+            """
+            Создание фрейма для кнопок.
+            :return:
+            """
+            self.frm_btn = Frame(self, border=2, relief='sunken')
             self.frm_btn.pack(side='bottom', fill='x')
-            
-            self.btnCancel=Button(self.frm_btn, text=self.lang['win_idc_cancel'], command=self.root.control.winIDC_cancel)
-            self.btnCancel.pack(side='right')
-            
-            self.btnReset=Button(self.frm_btn, text=self.lang['win_idc_reset'])
-            self.btnReset.pack(side='right')
-            
-            self.btn_ok=Button(self.frm_btn, text=' Ok ', command=self.lang['win_idc_image_create'])
+
+            self.btn_cancel = Button(self.frm_btn,
+                                    text=self.lang['win_idc_cancel'],
+                                    command=self.root.control.win_idc_cancel)
+            self.btn_cancel.pack(side='right')
+
+            self.btn_reset = Button(self.frm_btn,
+                                   text=self.lang['win_idc_reset'])
+            self.btn_reset.pack(side='right')
+
+            self.btn_ok = Button(self.frm_btn, text=' Ok ',
+                                 command=self.lang['win_idc_image_create'])
             self.btn_ok.pack(side='right')
-            
+
         def create_frmIDC():
-            self.frm_idc=Frame(self, border=2, relief='sunken')
+            """
+            Создание фрейма дискового контроллера.
+            :return:
+            """
+            self.frm_idc = Frame(self, border=2, relief='sunken')
             self.frm_idc.pack(side='top', fill='both', expand=1)
-            
-            self.frm_disk0=ClsFrmDiskSelect(master=self.frm_idc, root=self.root, text='Disk-0:')
-            self.frm_disk1=ClsFrmDiskSelect(master=self.frm_idc, root=self.root, text='Disk-1:', path='None')
-            self.frm_disk2=ClsFrmDiskSelect(master=self.frm_idc, root=self.root, text='Disk-2:', path='None')
-            self.frm_disk3=ClsFrmDiskSelect(master=self.frm_idc, root=self.root, text='Disk-3:', path='None')
-            
-        self.root=root
-        self.lang=root.res.lang_str.lang_dict
+
+            self.frm_disk0 = ClsFrmDiskSelect(master=self.frm_idc,
+                                              root=self.root, text='Disk-0:')
+            self.frm_disk1 = ClsFrmDiskSelect(master=self.frm_idc,
+                                              root=self.root, text='Disk-1:',
+                                              path='None')
+            self.frm_disk2 = ClsFrmDiskSelect(master=self.frm_idc,
+                                              root=self.root, text='Disk-2:',
+                                              path='None')
+            self.frm_disk3 = ClsFrmDiskSelect(master=self.frm_idc,
+                                              root=self.root, text='Disk-3:',
+                                              path='None')
+
+        self.root = root
+        self.lang = root.res.lang_str.lang_dict
         create_self()
-        create_frmBtn()
+        create_frm_btn()
         create_frmIDC()
-    
+
     def show(self):
+        """
+        Показ окна интерфейса дискового контролеера.
+        :return:
+        """
         self.state('normal')
         # показать поверх всех с фокусом
         self.focus_set()
         self.grab_set()
         self.wait_window()
-        
+
     def destroy(self):
+        """
+        Скрытие окна интерфейса дискового контроллера.
+        :return:
+        """
         self.state('withdrawn')
         self.grab_release()
-        
+
     def win_exit(self):
+        """
+        Уничтожение окна интерфейса дискового контроллера.
+        при закрытии всей программы.
+        :return:
+        """
         self.destroy()
