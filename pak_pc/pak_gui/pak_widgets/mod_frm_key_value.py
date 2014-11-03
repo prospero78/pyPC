349e89dc3f5dba31b4e3241a28358830f2a23ee8
prospero78 <prospero.78.su@gmail.com>
2014-11-03 17:54:51 +0200

    0.1284 FIX: исправлено форматирование кода. Рейтинг PyLint = 9.57/10.)

--- pak_pc/pak_gui/pak_widgets/mod_frm_key_value.py
+++ pak_pc/pak_gui/pak_widgets/mod_frm_key_value.py
@@ -1,34 +1,50 @@
 # -*- coding: utf8 -*-
-'''
+"""
 Модуль отображения регистра программного счётчика.
 Будет показывать содержимое и флаги.
-'''
+"""
 
 from Tkinter import Frame, Label, Entry
 
+
 class ClsFrmKeyValue(Frame):
+    """
+    Класс обеспечивает работу с парой ключ:значение.
+    """
     def __init__(self, root=None, key='Key', value='None'):
-        self.root=root
+        """
+        Конструтирует пару ключ:значение.
+        :param root:
+        :param key:
+        :param value:
+        :return:
+        """
+        self.root = root
         Frame.__init__(self, master=root, border=2, relief='ridge')
         self.pack(fill='x', side='top')
-        
-        self.lblKey=Label(self, text=key, border=1, relief='ridge', width=14)
-        self.lblKey.pack(side='left', fill='x', expand=1)
-        
-        self.entVal=Entry(self, border=1, relief='groove')
-        self.entVal.pack(side='left', fill='x', expand=1)
-        self.entVal.delete(0,'end')
-        self.entVal.insert(0,value)
-    
-    def get_val(self):
-        return self.entVal.get()
 
-        
-if __name__=='__main__':
+        self.lbl_key = Label(self, text=key, border=1, relief='ridge', width=14)
+        self.lbl_key.pack(side='left', fill='x', expand=1)
+
+        self.ent_val = Entry(self, border=1, relief='groove')
+        self.ent_val.pack(side='left', fill='x', expand=1)
+        self.ent_val.delete(0, 'end')
+        self.ent_val.insert(0, value)
+
+    def get_val(self):
+        """
+        Возвращает значение ключа из пары ключ:значение.
+        :return:
+        """
+        return self.ent_val.get()
+
+
+if __name__ == '__main__':
     from Tkinter import Tk
-    root=Tk()
-    root.title('test frmreg_pc')
-    root.minsize(300,200)
-    reg=ClsFrmRegPC(root=root)
-    reg.lbl_name['text']='reg_pc'
-    root.mainloop()
+
+    WIN_MAIN = Tk()
+    WIN_MAIN.title('test frmreg_pc')
+    WIN_MAIN.minsize(300, 200)
+    REG = ClsFrmKeyValue(root=WIN_MAIN)
+    REG.lbl_key['text'] = 'reg_pc'
+    WIN_MAIN.mainloop()
