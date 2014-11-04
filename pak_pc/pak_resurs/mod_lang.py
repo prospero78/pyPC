f50b2498116e7713ab3bc8b1c1d24de2421346d2
prospero78 <prospero.78.su@gmail.com>
2014-11-04 09:28:56 +0200

    0.1297 FIX: исправлено форматирование кода. Рейтинг PyLint = 9.17/10.

--- pak_pc/pak_resurs/mod_lang.py
+++ pak_pc/pak_resurs/mod_lang.py
@@ -1,16 +1,18 @@
 # -*- coding: utf8 -*-
-'''
+"""
 Модуль является базовым классом для для классов языков.
-'''
+"""
+
 
 class ClsLang(object):
-    '''
+    """
     Базовый класс для всех классов языков.
-    '''
+    """
+
     def __init__(self, lang='ru'):
-        '''
+        """
         Корневой класс для всех классов языков.
-        '''
+        """
         self.vers = 2
         self.__lang = lang
 
@@ -19,17 +21,17 @@ class ClsLang(object):
 
     @property
     def lang(self):
-        '''
+        """
         Возвращает установленный язык.
-        '''
+        """
         return self.__lang
 
     @lang.setter
     def lang(self, val=None):
-        ''''
+        """'
         Устанавливает текущий язык.
         Если значение неприемлимое -- ничего не делает.
-        '''
+        """
         if val == None or val == '':
             print 'ClsLang.lang_setter(): invalid val language!'
         else:
