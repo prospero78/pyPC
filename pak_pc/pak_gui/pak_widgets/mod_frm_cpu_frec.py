6e3c5fb7ce73934f97753a9dda95d161d4f12540
prospero78 <prospero.78.su@gmail.com>
2014-11-04 09:40:10 +0200

    0.1231 FIX: исправлено форматирование кода. Рейтинг PyLint = 6.00/10.

--- pak_pc/pak_gui/pak_widgets/mod_frm_cpu_frec.py
+++ pak_pc/pak_gui/pak_widgets/mod_frm_cpu_frec.py
@@ -1,11 +1,20 @@
 # -*- coding: utf8 -*-
-'''
+"""
 Модуль отображения текущей частоты работы процессора.
 Довольно важная вещь. )
-'''
+"""
 
-from mod_frm_key_value import ClsFrmKeyValue
+from pak_pc.pak_gui.pak_widgets.mod_frm_key_value import ClsFrmKeyValue
+
 
 class ClsFrmCpuFreq(ClsFrmKeyValue):
+    """
+    Фрейм отображения частоты ЦП.
+    """
     def __init__(self, root=None):
+        """
+        Создание фрейма для отображения частоты ЦП.
+        :param root:
+        :return:
+        """
         ClsFrmKeyValue.__init__(self, root=root, key='Frec', value='0 Hz')
