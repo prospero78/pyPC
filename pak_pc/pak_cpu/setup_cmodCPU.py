0da5f2e10d95d3e6480e8182f4cc760a360290c4
prospero78 <prospero.78.su@gmail.com>
2014-11-04 10:33:59 +0200

    0.1327 FIX: исправлено форматирование кода. Рейтинг PyLint = 7.14/10

--- pak_pc/pak_cpu/setup_cmodCPU.py
+++ pak_pc/pak_cpu/setup_cmodCPU.py
@@ -1,11 +1,13 @@
 from distutils.core import setup
 from distutils.extension import Extension
+
 from Cython.Distutils import build_ext
 import Cython.Compiler.Options as cOptions
-cOptions.annotate=True
 
+
+cOptions.annotate = True
 
 setup(
-    cmdclass = {'build_ext': build_ext},
-    ext_modules = [Extension("cmodCPU", ["cmodCPU.pyx"])]
+    cmdclass={'build_ext': build_ext},
+    ext_modules=[Extension("cmodCPU", ["cmodCPU.pyx"])]
 )
