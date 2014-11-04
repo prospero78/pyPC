ef2d687ecbad537709f3aec0cda1d2689cb6b335
prospero78 <prospero.78.su@gmail.com>
2014-11-04 09:58:38 +0200

    0.1237 FIX: исправлено форматирование кода. Рейтинг PyLint = 8.39/10.

--- pak_pc/pak_gui/pak_widgets/mod_frm_reg.py
+++ pak_pc/pak_gui/pak_widgets/mod_frm_reg.py
@@ -1,46 +1,57 @@
 # -*- coding: utf8 -*-
-'''
+"""
 Модуль отображения регистра.
 Будет показывать содержимое и флаги.
-'''
+"""
 
 from Tkinter import Frame, Label
 
+
 class ClsFrmReg(Frame):
+    """
+    Фрейм регистро
+    """
     def __init__(self, root=None):
-        self.root=root
+        """
+        Создаёт фрейм регистра.
+        :param root:
+        :return:
+        """
+        self.root = root
         Frame.__init__(self, master=root, border=2, relief='ridge')
         self.pack(fill='x', side='top')
-        
-        self.lbl_name=Label(self, text='RegXX', border=1, relief='ridge')
+
+        self.lbl_name = Label(self, text='RegXX', border=1, relief='ridge')
         self.lbl_name.pack(side='left', fill='x')
-        
-        self.lbl_val=Label(self, text='00', border=1, relief='groove')
+
+        self.lbl_val = Label(self, text='00', border=1, relief='groove')
         self.lbl_val.pack(side='left')
-        
-        self.lblFlagZ=Label(self, text='Z', border=1, relief='ridge')
-        self.lblFlagZ.pack(side='left', fill='x')
-        
-        self.lblValZ=Label(self, text='0', border=1, relief='groove')
-        self.lblValZ.pack(side='left', fill='x')
-        
-        self.lblFlagO=Label(self, text='O', border=1, relief='ridge')
-        self.lblFlagO.pack(side='left', fill='x')
-        
-        self.lblValO=Label(self, text='0', border=1, relief='groove')
-        self.lblValO.pack(side='left', fill='x')
-        
-        self.lblFlagC=Label(self, text='C', border=1, relief='ridge')
-        self.lblFlagC.pack(side='left', fill='x')
-        
-        self.lblValC=Label(self, text='0', border=1, relief='groove')
-        self.lblValC.pack(side='left', fill='x')
-        
-if __name__=='__main__':
+
+        self.lbl_flag_z = Label(self, text='Z', border=1, relief='ridge')
+        self.lbl_flag_z.pack(side='left', fill='x')
+
+        self.lbl_val_z = Label(self, text='0', border=1, relief='groove')
+        self.lbl_val_z.pack(side='left', fill='x')
+
+        self.lbl_flag_o = Label(self, text='O', border=1, relief='ridge')
+        self.lbl_flag_o.pack(side='left', fill='x')
+
+        self.lbl_val_o = Label(self, text='0', border=1, relief='groove')
+        self.lbl_val_o.pack(side='left', fill='x')
+
+        self.lbl_flag_c = Label(self, text='C', border=1, relief='ridge')
+        self.lbl_flag_c.pack(side='left', fill='x')
+
+        self.lbl_val_c = Label(self, text='0', border=1, relief='groove')
+        self.lbl_val_c.pack(side='left', fill='x')
+
+
+if __name__ == '__main__':
     from Tkinter import Tk
-    root=Tk()
+
+    root = Tk()
     root.title('test frm_reg')
-    root.minsize(300,200)
-    reg=ClsFrmReg(root=root)
-    reg.lbl_name['text']='123'
+    root.minsize(300, 200)
+    reg = ClsFrmReg(root=root)
+    reg.lbl_name['text'] = '123'
     root.mainloop()
