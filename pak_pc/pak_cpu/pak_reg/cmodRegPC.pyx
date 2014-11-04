0469ccb1ddb3045c472888dde8d15a7b61d2215f
prospero78 <prospero.78.su@gmail.com>
2014-11-04 10:31:39 +0200

    0.1326 FIX: исправлено форматирование кода.

--- pak_pc/pak_cpu/pak_reg/cmodRegPC.pyx
+++ pak_pc/pak_cpu/pak_reg/cmodRegPC.pyx
@@ -5,17 +5,17 @@
 '''
 
 cdef class clsRegPC:
-    cdef readonly int val # значение счётчика
-    cdef readonly int max_adr # максимальное значение адреса
+    cdef readonly int val  # значение счётчика
+    cdef readonly int max_adr  # максимальное значение адреса
     def __init__(self, root=None, int max_adr=0xFFFF):
-        self.root=root
-        self.max_adr=max_adr
+        self.root = root
+        self.max_adr = max_adr
         # сбросить регистр в ноль
-        self.val=0
-        
+        self.val = 0
+
     def get(self):
         return self.val
-        
+
     def set(self, int new_val):
-        if new_val<self.max_adr and new_val>=0:
-            self.val=new_val
+        if new_val < self.max_adr and new_val >= 0:
+            self.val = new_val
