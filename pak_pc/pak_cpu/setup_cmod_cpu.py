1b0e4290136e647e49e0f9da681aaa4439b2674d
prospero78 <prospero.78.su@gmail.com>
2014-11-04 10:50:33 +0200

    0.1354 ADD: исправление имён по рекомендации PyLint.

---
+++ pak_pc/pak_cpu/setup_cmod_cpu.py
@@ -0,0 +1,17 @@
+# -*- coding: utf8 -*-
+"""
+Модуль компилирует модуль ЦП.
+"""
+from distutils.core import setup
+from distutils.extension import Extension
+
+from Cython.Distutils import build_ext
+import Cython.Compiler.Options as cOptions
+
+
+cOptions.annotate = True
+
+setup(
+    cmdclass={'build_ext': build_ext},
+    ext_modules=[Extension("cmodCPU", ["cmodCPU.pyx"])]
+)
