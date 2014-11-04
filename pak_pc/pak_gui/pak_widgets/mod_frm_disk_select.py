6f5b327d4825f328356d320022ddb0972f0a7593
prospero78 <prospero.78.su@gmail.com>
2014-11-04 09:42:36 +0200

    0.1232 FIX: исправлено форматирование кода. Рейтинг PyLint = 4.64/10.

--- pak_pc/pak_gui/pak_widgets/mod_frm_disk_select.py
+++ pak_pc/pak_gui/pak_widgets/mod_frm_disk_select.py
@@ -1,47 +1,74 @@
 # -*- coding: utf8 -*-
-'''
+"""
 Инициализация пакета виджета выбора диска.
-'''
+"""
 
 from Tkinter import Frame, Label, Button
 from tkFileDialog import askopenfilename
 
+
 class ClsFrmDiskSelect(Frame):
+    """
+    Фрейм выбора диска.
+    """
     def __init__(self,
-                master=None,
-                root=None,
-                text='Disk-0:',
-                path='./data/default.dsk'):
+                 master=None,
+                 root=None,
+                 text='Disk-0:',
+                 path='./data/default.dsk'):
+        """
+        Создаёт фрейм для выбора диска.
+        :param master:
+        :param root:
+        :param text:
+        :param path:
+        :return:
+        """
         def create_self():
+            """
+            Создание самого фрейма.
+            :return:
+            """
             def open_disk(event=''):
-                d=askopenfilename(defaultextension='.dsk',
-                        initialfile='./data/default.dsk',
-                        filetypes=[('Disk images','.dsk'),],
-                        title=self.root.res.winIDC_OpenDiskImage)
+                """
+                Запрос на отрытие файла-диска.
+                :param event:
+                :return:
+                """
+                d = askopenfilename(defaultextension='.dsk',
+                                    initialfile='./data/default.dsk',
+                                    filetypes=[('Disk images', '.dsk'), ],
+                                    title=self.root.res.winIDC_OpenDiskImage)
                 self.root.gui.win_idc.lift()
                 self.root.gui.win_idc.focus_get()
+
             Frame.__init__(self, master=self.master)
             self.pack(side='top', fill='x')
-            
-            self.lblPath=Label(self, text=self.text, border=2, relief='raised')
+
+            self.lblPath = Label(self, text=self.text, border=2,
+                                 relief='raised')
             self.lblPath.pack(side='left')
-            
-            self.lblDiskPath=Label(self, text=self.path, anchor='w', border=2, relief='groove')
+
+            self.lblDiskPath = Label(self, text=self.path, anchor='w', border=2,
+                                     relief='groove')
             self.lblDiskPath.pack(side='left', fill='both', expand=1)
-            
-            self.btnCreate=Button(self, text=self.lang['win_idc_image_create'], command=self.root.control.create_disk)
+
+            self.btnCreate = Button(self,
+                                    text=self.lang['win_idc_image_create'],
+                                    command=self.root.control.create_disk)
             self.btnCreate.pack(side='left')
-            
-            self.btnOpen=Button(self, text=self.lang['win_idc_open'], command=open_disk)
+
+            self.btnOpen = Button(self, text=self.lang['win_idc_open'],
+                                  command=open_disk)
             self.btnOpen.pack(side='left')
-            
-            self.btnClear=Button(self, text=self.lang['win_idc_image_unpath'])
+
+            self.btnClear = Button(self, text=self.lang['win_idc_image_unpath'])
             self.btnClear.pack(side='left')
-            
-            
-        self.root=root
-        self.lang=root.res.lang_str.lang_dict
-        self.master=master
-        self.path=path
-        self.text=text
+
+
+        self.root = root
+        self.lang = root.res.lang_str.lang_dict
+        self.master = master
+        self.path = path
+        self.text = text
         create_self()
