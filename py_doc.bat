e987c9aede6dbde631dfc19137ddbdeb779d1d0a
prospero78 <prospero.78.su@gmail.com>
2014-11-05 15:04:12 +0200

    0.1380 ADD/FIX: добавленs строки комментариев для понимания а что собственно делать.

--- py_doc.bat
+++ py_doc.bat
@@ -1,1 +1,4 @@
+echo off
+echo Open your web-brouser in localhost:8000 for reading web-doc your project.
+echo Press [Control-C] for [end].
 d:\PortablePython275\App\python D:\PortablePython275\App\Lib\pydoc.py -p 8000 -w doc.html main
