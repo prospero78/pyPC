c43d6b5d2094d2d8174003f15fa84d2e96672969
prospero78 <prospero.78.su@gmail.com>
2014-11-05 08:50:46 +0200

    0.1361 ADD: исправление форматирования по рекомендации PyLint.

--- pak_pc/pak_cpu/pak_video/mod_video.py
+++ pak_pc/pak_cpu/pak_video/mod_video.py
@@ -17,6 +17,7 @@
 import multiprocessing
 from time import sleep
 
+
 class ClsVideo(multiprocessing.Process):
     def __init__(self):
         '''
@@ -25,52 +26,52 @@ class ClsVideo(multiprocessing.Process):
         '''
         # создание отдельного процесса
         multiprocessing.Process.__init__(self)
-        self.daemon=True
-        
+        self.daemon = True
+
         # очередь для получения команд
-        self.vcom=multiprocessing.Queue()
+        self.vcom = multiprocessing.Queue()
         # очередь для отправки информации назад в железную часть
-        self.vinfo=multiprocessing.Queue()
-        
+        self.vinfo = multiprocessing.Queue()
+
         # очередь для отправки информации для прорисовки
-        self.vout=multiprocessing.Queue()
-        
+        self.vout = multiprocessing.Queue()
+
         # максимальный режим видеокарты
-        self.mode_max=0
+        self.mode_max = 0
         # текущий режим видеокарты
-        self.mode_current=0
-        
-        self.command=0
-        self.buf=''
-        self.adr={}
-    
+        self.mode_current = 0
+
+        self.command = 0
+        self.buf = ''
+        self.adr = {}
+
     def run(self):
         '''
         Вызывается при запуске отдельного процесса.
         '''
         self.clear_screen()
         while True:
-            #print("The process VideoCard!")
+            # print("The process VideoCard!")
             if not self.vcom.empty():
-                com=self.vcom.get()
+                com = self.vcom.get()
                 print 'clsVideo.run(): com=', com
                 if com.has_key('com'):
-                    com=com['com']
+                    com = com['com']
                     if com.has_key('set_mode'):
-                        com=com['set_mode']
+                        com = com['set_mode']
                         print '       com=', com
                         self.set_current_mode(mode=com)
                     elif com.has_key('get_max_mode'):
-                        info={'max_mode':self.mode_max}
+                        info = {'max_mode': self.mode_max}
                         self.vinfo.put(info)
                     elif com.has_key('clear_screen'):
                         self.clear_screen()
                     elif com.has_key('fill_screen'):
-                        com=com['fill_screen']
+                        com = com['fill_screen']
                         print '       com=', com
                         self.fill_screen(sym=com)
             sleep(0.1)
-    
+
     def clear_screen(self):
         '''
         Очищает терминал от текста.
@@ -78,41 +79,41 @@ class ClsVideo(multiprocessing.Process):
         '''
         print 'video: clear_screen()'
         self.fill_screen()
-        
+
     def fill_screen(self, sym=35):
         '''
         Заливка экрана заданным символом.
         '''
         print 'clsVideo.fill_screen()'
-        a=''
-        sym=chr(35)
+        a = ''
+        sym = chr(35)
         for i in xrange(0, 40):
-            for i1 in xrange(0,81):
-                if i1==80:
-                    self.adr[i*i1]='\n'
-                    a+='\n'
+            for i1 in xrange(0, 81):
+                if i1 == 80:
+                    self.adr[i * i1] = '\n'
+                    a += '\n'
                 else:
-                    self.adr[i*i1]=sym
-                    a+=sym
+                    self.adr[i * i1] = sym
+                    a += sym
         self.vout.put(a)
         print a
-    
+
     def get_max_mode(self):
         '''
         Возвращает допустимый предельный номер режима экрана.
         '''
         print 'video: get_max_mode()'
         return self.mode_max
-        
+
     def set_current_mode(self, mode=None):
         '''
         Устанавливает новый режим экрана.
         '''
         print 'clsVideo.set_current_mode(): mode=', mode
-        if mode>self.mode_max:
-            self.mode_current=self.mode_max
-        elif mode<0 or mode==None:
-            self.mode_current=0
+        if mode > self.mode_max:
+            self.mode_current = self.mode_max
+        elif mode < 0 or mode == None:
+            self.mode_current = 0
         else:
-            self.mode_current=mode
+            self.mode_current = mode
         return self.mode_current
