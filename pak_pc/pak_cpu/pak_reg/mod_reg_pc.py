f966266c0957eae0c179f1e78cfb08e59c4c4345
prospero78 <prospero.78.su@gmail.com>
2014-11-05 08:51:13 +0200

    0.1363 ADD: исправление форматирования по рекомендации PyLint.

--- pak_pc/pak_cpu/pak_reg/mod_reg_pc.py
+++ pak_pc/pak_cpu/pak_reg/mod_reg_pc.py
@@ -3,10 +3,12 @@
 Класс программного счётчика.
 '''
 
+
 class ClsRegPC:
     '''
     Класс программного счётчика обеспечивает учёт текущей инструкции системы.
     '''
-    def __init__(self, val=0, max_adr=2**24):
+
+    def __init__(self, val=0, max_adr=2 ** 24):
         self.val = val
         self.max_adr = max_adr
