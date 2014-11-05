bdce55bcb29626658f60503fa717d455c561b13f
prospero78 <prospero.78.su@gmail.com>
2014-11-05 08:51:24 +0200

    0.1364 ADD: исправление форматирования по рекомендации PyLint.

--- pak_pc/pak_cpu/pak_reg/mod_reg_bp.py
+++ pak_pc/pak_cpu/pak_reg/mod_reg_bp.py
@@ -3,38 +3,39 @@
 Класс регистра для остановки процессора.
 '''
 
+
 class ClsRegBP:
-    def __init__(self, adr_break=0, act=0, adr_proc=0):
+    def __init__(self, adr_break=0, flag_act=0, adr_proc=0):
         '''
         adr_break -- aдрес, на который бряк
-        act -- активность регистра
+        flag_act -- активность регистра
         adr_proc -- куда перекинуть управление
         '''
-        self.adr_break=adr_break
-        self.adr_old=0
-        self.act=act
-        self.adr_proc=adr_proc
-    
+        self.adr_break = adr_break
+        self.adr_old = 0
+        self.flag_act = flag_act
+        self.adr_proc = adr_proc
+
     def get_adr_break(self):
         return self.adr_break
-        
+
     def set_adr_break(self, adr_break):
-        self.adr_break=adr_break
-        
+        self.adr_break = adr_break
+
     def get_adr_proc(self):
         return self.adr_proc
-        
+
     def set_adr_proc(self, adr_proc):
-        self.adr_proc=adr_proc
-        
+        self.adr_proc = adr_proc
+
     def get_act(self):
-        return self.act
-        
-    def set_act(self, act):
-        self.act=act
-    
+        return self.flag_act
+
+    def set_act(self, flag_act):
+        self.flag_act = flag_act
+
     def get_adr_old(self):
         return self.adr_old
-        
+
     def set_adr_old(self, adr_old):
-        self.adr_old=adr_old
+        self.adr_old = adr_old
