3983961eef2b5b383f06ddb6a3537b67dadd5cfa
prospero78 <prospero.78.su@gmail.com>
2014-11-05 08:51:54 +0200

    0.1367 ADD: исправление форматирования по рекомендации PyLint.

--- pak_pc/pak_gui/pak_widgets/mod_frm_reg_sp.py
+++ pak_pc/pak_gui/pak_widgets/mod_frm_reg_sp.py
@@ -5,35 +5,37 @@
 
 from Tkinter import Frame, Label
 
+
 class ClsFrmRegSP(Frame):
     def __init__(self, root=None):
-        self.root=root
+        self.root = root
         Frame.__init__(self, master=root, border=2, relief='ridge')
         self.pack(fill='x', side='top')
-        
-        self.lbl_name=Label(self, text='reg_sp', border=1, relief='ridge')
-        self.lbl_name.pack(side='left', fill='x')
-        
-        self.lblAdr=Label(self, text='adr', border=1, relief='groove', bg='gray')
-        self.lblAdr.pack(side='left')
-        
-        self.lblAdrVal=Label(self, text='0000', border=1, relief='groove')
-        self.lblAdrVal.pack(side='left')
-        
-        self.lbl_val=Label(self, text='val', border=1, relief='groove', bg='gray')
-        self.lbl_val.pack(side='left')
-        
-        self.lblValVal=Label(self, text='0000', border=1, relief='groove')
-        self.lblValVal.pack(side='left')
-        
-        
 
-        
-if __name__=='__main__':
+        self.lbl_name = Label(self, text='reg_sp', border=1, relief='ridge')
+        self.lbl_name.pack(side='left', fill='x')
+
+        self.lblAdr = Label(self, text='adr', border=1, relief='groove',
+                            bg='gray')
+        self.lblAdr.pack(side='left')
+
+        self.lblAdrVal = Label(self, text='0000', border=1, relief='groove')
+        self.lblAdrVal.pack(side='left')
+
+        self.lbl_val = Label(self, text='val', border=1, relief='groove',
+                             bg='gray')
+        self.lbl_val.pack(side='left')
+
+        self.lblValVal = Label(self, text='0000', border=1, relief='groove')
+        self.lblValVal.pack(side='left')
+
+
+if __name__ == '__main__':
     from Tkinter import Tk
-    root=Tk()
+
+    root = Tk()
     root.title('test frm_reg_sp')
-    root.minsize(300,200)
-    reg=ClsFrmRegPC(root=root)
-    reg.lbl_name['text']='reg_sp'
+    root.minsize(300, 200)
+    reg = ClsFrmRegPC(root=root)
+    reg.lbl_name['text'] = 'reg_sp'
     root.mainloop()
