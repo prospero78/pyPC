6332c0620010f3c968ba7eff364b3031b541db56
prospero78 <prospero.78.su@gmail.com>
2014-11-06 12:09:04 +0200

    0.1346 ADD:добавлен класс окна ClsWinTop. Рейтинг PyLint -- 5.00/10.

--- pak_pc/pak_gui/pak_widgets/mod_top_win.py
+++ pak_pc/pak_gui/pak_widgets/mod_top_win.py
@@ -1,3 +1,21 @@
 # /usr/bin/python27
 # -*- coding: utf8 -*-
+"""
+Модуль содержит класс отображающий окно поверх всех окон.
+"""
 __author__ = 'ARM'
+
+from pak_pc.pak_gui.pak_widgets.mod_win import ClsWin
+
+class ClsTopWin(ClsWin):
+    """
+    Класс созаёт окно поверх всех окон.
+    """
+
+    def __init__(self, title='ClsWinTop'):
+        """
+        Создаёт класс окна для показа поверх всех окон.
+        :type title: string
+        :param title:
+        """
+        self.title = title
