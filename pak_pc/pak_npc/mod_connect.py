6698100fb2c0ebad84ec91688c284d758f43e72c
prospero78 <prospero.78.su@gmail.com>
2014-11-10 16:56:30 +0200

    0.1472 ADD: добавлен код для хранения сетевых настроек подключения.

--- pak_pc/pak_npc/mod_connect.py
+++ pak_pc/pak_npc/mod_connect.py
@@ -3,10 +3,17 @@
 Модуль предоставляет класс для сохранения данных о подключении.
 """
 
-class ClsStoreNetConnect:
+from Queue import Queue
+
+class ClsNetStor:
     """
     Класс хранит данные о подключении.
     """
     def __init__(self, adr = None, port = None):
+        # сетевые адрес и порт
         self.adr = adr
         self.port = port
+        
+        # создание очередей для приёма и отправки информации в дочерние процессы
+        self.out = Queue()
+        self.in_ = Queue()
