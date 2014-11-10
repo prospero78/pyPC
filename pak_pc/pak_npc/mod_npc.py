a1be28f60ed2d483765a97c927b9f3b924176bd9
prospero78 <prospero.78.su@gmail.com>
2014-11-10 12:44:18 +0200

    0.1458 ADD: добавление модуля сетевого взаимодействия процессов.

---
+++ pak_pc/pak_npc/mod_npc.py
@@ -0,0 +1,68 @@
+# -*- coding: utf8 -*-
+"""
+Модуль предоставляющий класс для работы хост-процесса с
+множеством процессов по сетевому стеку. Это будет
+универсальное и переносимое решение.
+"""
+
+import socket
+from threading import Thread
+from Queue import Queue
+from time import sleep
+
+
+class ClsIPC(Thread):
+    """
+    Класс обеспечивает взаимодействие различных частей python и cython кода через различные процессы.
+    Использование мультипроцессинга напрямую вызывает дикие проблемы.
+    """
+    def __init__ (self):
+        """
+        обеспечивает инициализацию класса сетевых межпроцессорных взаимодействий
+        :return: возвращает ссылку на себя.
+        """
+        Thread.__init__(self)
+        self.start()
+
+    def run(self):
+        """
+        Метод исполняется при запуске отдельного потока.
+        Метод зациклен.
+        """
+        
+        #--- определение сетевых подключений ---
+        HOST = "" # localhost
+        PORT = 58633
+        
+        # --- создание сервера для подключения
+        srv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #srv.settimeout(5)
+        
+        while 1:
+            try:
+                #--- запуск сервера ---
+                srv.bind((HOST, PORT))
+                
+                #--- счётчик циклов и цикл
+                count_loop = 0
+                while True:
+                    print 'Name = ', self.getName(), count_loop    # получить имя системы и счётчик циклов
+                    print "Слушаю порт".decode('utf8'), PORT
+                    srv.listen(1)  # сколько можно одновременно получить подключений на порт
+                    sock, addr = srv.accept() # ждём когда вылезит клиент
+                    while 1:
+                        pal = sock.recv(1024) # получить данные
+                        if not pal: # если данных нет -- не продолжать
+                            print 'Not reada net data'
+                        else:
+                            print "Получено от %s:%s:" % addr, pal
+                            lap = do_something(pal)
+                            print "Отправлено %s:%s:" % addr, lap
+                            sock.send(lap)
+                        sock.close()
+            finally:
+                sleep(1)
+                count_loop += 1
+
+if __name__=='__main__':
+    app=ClsIPC()
