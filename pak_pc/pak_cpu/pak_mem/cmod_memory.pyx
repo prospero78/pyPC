0fa30d53c7f74813d0afdb6e90213d23c64d811e
prospero78 <prospero.78.su@gmail.com>
2014-11-10 11:00:15 +0200

    0.1437 ADD: добавлена ситонизация кода.

--- pak_pc/pak_cpu/pak_mem/cmod_memory.pyx
+++ pak_pc/pak_cpu/pak_mem/cmod_memory.pyx
@@ -13,10 +13,13 @@ DEF MAX_ADR=2 ** 22
 
 cdef class ClsMemory:
     """
-    Описывает память в виде болчного устройства.
+    Описывает память в виде блочного устройства.
     """
     #cdef public Mem *mem
     cdef int adr[MAX_ADR]
+    cdef public int max_adr
+    cdef public int act_mem
+    
     def __init__(self, int max_adr=2 ** 22):
         """
         Начальная инициализация памяти.
@@ -26,14 +29,14 @@ cdef class ClsMemory:
         # максимально допустимое количество памяти
         self.max_adr = max_adr
         # текущее количество памяти
-        self.act_mem = 1024
+        self.act_mem = max_adr
         # инициализация памяти виртуального компьютера
         #self.adr = {}
         cdef int i = 0
         for i in xrange(0, self.act_mem):
             self.adr[i] = 0
 
-    def get_adr(self, adr):
+    cpdef int get_adr(self, int adr):
         """
         Возвращает значение ячейки памяти по адресу (для cython)
         :param adr:
@@ -41,7 +44,7 @@ cdef class ClsMemory:
         """
         return self.adr[adr]
 
-    def set_adr(self, adr, val):
+    cpdef set_adr(self, int adr, int val):
         """
         Устанавливает ячейку памяти в значение. (для cython)
         :param adr:
@@ -50,7 +53,7 @@ cdef class ClsMemory:
         """
         self.adr[adr] = val
 
-    def get_max_adr(self):
+    cpdef int get_max_adr(self):
         """
         Возвращает максимально-доступный адрес памяти.
         Выше его быть памяти в любом случае не может.
@@ -58,7 +61,7 @@ cdef class ClsMemory:
         """
         return self.max_adr
 
-    def add_memory(self):
+    cpdef add_memory(self):
         """
         Добавляет памяти блоками по 1024 ячейки (НЕ БАЙТА!!!)
         :return:
