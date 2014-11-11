c3e7fea3539919df1fbeaca944dbb6006a6f308e
prospero78 <prospero.78.su@gmail.com>
2014-11-11 12:17:38 +0200

    0.1479 ADD: добавлен код для сетевого взаимодействия.

--- pak_pc/pak_npc/mod_connect.py
+++ pak_pc/pak_npc/mod_connect.py
@@ -9,16 +9,31 @@ class ClsNetStore(object):
     """
     Класс хранит данные о подключении.
     """
-    def __init__(self, adr = 'localhost', port = None):
+    def __init__(self, adr='localhost', port=None):
         """
         Создаёт объект сетевого подключения
         :param adr: ip-адрес соединения
         :param port: сетевой порт.
         """
         # сетевые адрес и порт
-        self.adr = adr
+        self.conn = adr
         self.port = port
 
-        # создание очередей для приёма и отправки информации в дочерние процессы
+        # признак установленного подключения
+        self.__connecting = False
+
+        # создание очередей для приёма и отправки информации в дочерние
+        # процессы
         self.out = Queue()
         self.in_ = Queue()
+
+    def put (self, info=None):
+        """
+        Помещает информацию в исходящую очередь.
+        """
+        if (info is dict) and (len(info) > 1):
+            self.out.append(info)
+
+    @property
+    def connecing(self):
+        return self.__connecting
