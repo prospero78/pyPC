21d28dd5a7cdfec400eaa32f68f23119cc9fd346
prospero78 <prospero.78.su@gmail.com>
2014-11-11 11:32:47 +0200

    0.1477 ADD|FIX: массовые исправления кода. PyLint - 2.73/10.

--- pak_pc/pak_npc/mod_npc.py
+++ pak_pc/pak_npc/mod_npc.py
@@ -8,97 +8,100 @@
 import socket
 from threading import Thread
 from time import sleep
-from mod_connect import ClsNetStor
+from pak_pc.pak_npc.mod_connect import ClsNetStore
 
+#--- определение сетевых подключений ---
+HOST_VIDEO = "" # localhost
+
+# порт, на который должно прийти подключение от видеокарты.
+PORT_VIDEO = 58633
 
 class ClsNPC(Thread):
     """
-    Класс обеспечивает взаимодействие различных частей python и cython кода через различные процессы.
+    Класс обеспечивает взаимодействие различных частей python и cython
+    кода через различные процессы.
     Использование мультипроцессинга напрямую вызывает дикие проблемы.
     """
-    def __init__ (self):
+    def __init__(self):
         """
-        обеспечивает инициализацию класса сетевых межпроцессорных взаимодействий
+        обеспечивает инициализацию класса сетевых межпроцессорных
+        взаимодействий
         :return: возвращает ссылку на себя.
         """
-        
-        Thread.__init__(self)
 
-    def run(self):
-        """
-        Метод исполняется при запуске отдельного потока.
-        Метод зациклен.
-        """
-        
         def get_video_connect():
             """
             Устанавливает соединение видеокарты.
             """
-            #--- определение сетевых подключений ---
-            HOST = "" # localhost
-            PORT = 58633 # порт, на который должно прийти подключение от видеокарты.
-            
+
             # --- создание сервера для подключения
             srv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             #srv.settimeout(5)
-            
+
             #--- запуск сервера ---
-            srv.bind((HOST, PORT))
-            srv.listen(1)  # сколько можно одновременно получить подключений на порт
-            self.video_socket, self.video_addr = srv.accept() # ждём когда подкючится клиент видеокарты
+            srv.bind((HOST_VIDEO, PORT_VIDEO))
+            # сколько можно одновременно получить подключений на порт
+            srv.listen(1)
+            # ждём когда подкючится клиент видеокарты
+            self.video.port, self.video.adr = srv.accept()
 
         def get_cpu_connect():
             """
             Устанавливает соединение с ЦП.
             """
             pass
-        
+
+        Thread.__init__(self)
+        self.video = ClsNetStore()
+        self.cpu = ClsNetStore()
+
+        # -- признак подключения видеокарты ---
+        self.video_connect = False
+
+        # -- признак подключения ЦП ---
+        self.cpu_connect = False
+
+        # --- получить подключение от видеокарты ---
+        get_video_connect()
+        get_cpu_connect()
+
+    def run(self):
+        """
+        Метод исполняется при запуске отдельного потока.
+        Метод зациклен.
+        """
+
         def send_info():
             """
             Отправляет сообщения из очередей сообщений.
             """
             pass
-            
+
         def get_info():
             """
             Получает сообщения по сети и ставит в очередь обработки.
             """
-        
-        
-        # -- признак подключения видеокарты ---
-        self.video_connect = False
-        
-        # -- признак подключения ЦП ---
-        self.cpu_connect = False
-        
-        # --- данные по подключению видеокарты ---
-        self.net_video = ClsNetStor()
-        
-        # --- данные по подключению ЦП ---
-        self.net_cpu = ClsNetStor()
 
-        # --- получить подключение от видеокарты ---
-        get_video_connect()
-        
         #--- счётчик циклов и цикл
         count_loop = 0
-        
-            print 'Name = ', self.getName(), count_loop    # получить имя системы и счётчик циклов
-            print "Слушаю порт".decode('utf8'), PORT
-            
-            while 1:
-                pal = sock.recv(1024) # получить данные
-                if not pal: # если данных нет -- не продолжать
-                    print 'Not reada net data'
-                else:
-                    print "Получено от %s:%s:" % addr, pal
-                    lap = do_something(pal)
-                    print "Отправлено %s:%s:" % addr, lap
-                    sock.send(lap)
-                    sock.close()
-                    sleep(1)
-                    count_loop += 1
 
-if __name__=='__main__':
-    app=ClsNPC()
-    app.start()
+        # получить имя системы и счётчик циклов
+        print 'Name = ', self.getName(), count_loop
+        print "Слушаю порт".decode('utf8'), PORT_VIDEO
+
+        while 1:
+            pal = sock.recv(1024) # получить данные
+            if not pal: # если данных нет -- не продолжать
+                print 'Not reada net data'
+            else:
+                print "Получено от %s:%s:" % addr, pal
+                lap = do_something(pal)
+                print "Отправлено %s:%s:" % addr, lap
+                sock.send(lap)
+                sock.close()
+                sleep(1)
+                count_loop += 1
+
+if __name__ == '__main__':
+    APP = ClsNPC()
+    APP.start()
