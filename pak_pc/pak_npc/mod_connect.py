dad82e1a1d801982169fa8296680b16414c2dd7c
prospero78 <prospero.78.su@gmail.com>
2014-11-11 11:31:56 +0200

    0.1476 ADD: добавлена документация для методов класса.

--- pak_pc/pak_npc/mod_connect.py
+++ pak_pc/pak_npc/mod_connect.py
@@ -5,15 +5,20 @@
 
 from Queue import Queue
 
-class ClsNetStor:
+class ClsNetStore(object):
     """
     Класс хранит данные о подключении.
     """
-    def __init__(self, adr = None, port = None):
+    def __init__(self, adr = 'localhost', port = None):
+        """
+        Создаёт объект сетевого подключения
+        :param adr: ip-адрес соединения
+        :param port: сетевой порт.
+        """
         # сетевые адрес и порт
         self.adr = adr
         self.port = port
-        
+
         # создание очередей для приёма и отправки информации в дочерние процессы
         self.out = Queue()
         self.in_ = Queue()
