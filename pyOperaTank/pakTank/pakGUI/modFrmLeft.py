576c6c522d8140dd5923c8d7f553fb332b23e19d
prospero78 <prospero.78.su@gmail.com>
2014-11-13 11:20:33 +0200

    0.1512 ADD: добавлен модуль левого фрейма со своим танком и всякими измерялками.

---
+++ pyOperaTank/pakTank/pakGUI/modFrmLeft.py
@@ -0,0 +1,25 @@
+# -*- coding: utf8 -*-
+"""
+Левый фрейм со своим танком и всякими измерялками.
+"""
+
+from Tkinter import Frame
+
+class clsFrmLeft(Frame):
+    """
+    Левый фрейм содержит всякие расчётные данные по своему танку.
+    """
+    def __init__(self,
+                 root=None,
+                 my_tank=None):
+        Frame.__init__(self, master=root, border=3, relief='sunken')
+        self.pack(side='left', fill='y')
+        self.lfrMyTank = ClsLfrTank(root=self,
+                                    label = 'Мой танк',
+                                    tank=my_tank)
+        self.lfrMyTank.kvVer1 = ClsKeyValue(root=self.lfrMyTank,
+                                            key='Вероятность 1*',
+                                            value='?')
+        self.lfrMyTank.kvVer2 = ClsKeyValue(root=self.lfrMyTank,
+                                            key='Вероятность 2*',
+                                            value='?')
