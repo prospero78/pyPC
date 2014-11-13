25e84f99fbfc48ac3f5e2799cc75731f12e739f1
prospero78 <prospero.78.su@gmail.com>
2014-11-13 10:57:40 +0200

    0.1502 ADD: перенос кода из другого модуля.

--- pyOperaTank/pakTank/pakGUI/modWinMain.py
+++ pyOperaTank/pakTank/pakGUI/modWinMain.py
@@ -2,3 +2,23 @@
 """
 Инициализация пакета для танковой графики.
 """
+
+from Tkinter import Tk
+
+class clsWinMain(Tk):
+    """
+    Класс обеспечивает графику пользователя.
+    """
+    def __init__(self, my_tank=None, tank1=None, tank2=None):
+        Tk.__init__(self)
+        self.title('Калькулятор танков     ===build. 009===')
+        self.minsize(320, 240)
+        self.frmButton = ClsFrmButton(root=self)
+        self.frmLeft = ClsFrmLeft(root=self, my_tank=my_tank)
+        self.frmRight = ClsFrmRight(root=self, tank1=tank1, tank2=tank2)
+        self.after(1000, self.update_sound)
+        self.mainloop()
+        
+    def update_sound(self):
+        print 'aaa'
+        self.after(1000, self.update_sound)
