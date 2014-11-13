a96a54099db42371d458256f9ebb0858950de8e1
prospero78 <prospero.78.su@gmail.com>
2014-11-13 11:42:46 +0200

    0.1520 ADD: добавление модуля для отображения пары "ключ:значение".

---
+++ pyOperaTank/pakTank/pakGUI/modFrmKeyValue.py
@@ -0,0 +1,22 @@
+# -*- coding: utf8 -*-
+"""
+Фрейм отображающий пару "ключ:значение".
+"""
+
+class ClsKeyValue(Frame):
+    """
+    Класс обеспечивает пару "ключ:значение"
+    """
+    def __init__(self, root=None, key='key', value='None'):
+        Frame.__init__(self, master=root, border=3, relief='groove')
+        self.pack(side='top', fill='x')
+        
+        self.key=key
+        self.value=value
+        
+        self.lblKey = Label(master=self, text=key)
+        self.lblKey.pack(side='left', fill='x')
+        
+        self.entValue = Entry(master=self)
+        self.entValue.pack(side='right')
+        self.entValue.insert(0, value)
