bb1824a69fb23a8d32ccdc377054a6cf85744117
prospero78 <prospero.78.su@gmail.com>
2014-11-13 11:44:22 +0200

    0.1521 FIX: исправление имени класса.

--- pyOperaTank/pakTank/pakGUI/modLfrTank.py
+++ pyOperaTank/pakTank/pakGUI/modLfrTank.py
@@ -20,8 +20,8 @@ class clsLfrTank(LabelFrame):
                             font='Consolas 12 bold')
         self.pack(side='top', fill='y')
         
-        self.kvAtak = ClsKeyValue(root=self, key='Атака', value=tank[0])
-        self.kvBron = ClsKeyValue(root=self, key='Броня', value=tank[1])
-        self.kvBron = ClsKeyValue(root=self, key='Точность', value=tank[2])
-        self.kvBron = ClsKeyValue(root=self, key='Прочность', value=tank[3])
-        self.kvMosh = ClsKeyValue(root=self, key='Мощность*', value='?')
+        self.kvAtak = clsKeyValue(root=self, key='Атака', value=tank[0])
+        self.kvBron = clsKeyValue(root=self, key='Броня', value=tank[1])
+        self.kvBron = clsKeyValue(root=self, key='Точность', value=tank[2])
+        self.kvBron = clsKeyValue(root=self, key='Прочность', value=tank[3])
+        self.kvMosh = clsKeyValue(root=self, key='Мощность*', value='?')
