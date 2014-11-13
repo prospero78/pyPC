e6b2bba7c87db0e72edf613e8b51c3f875f7340e
prospero78 <prospero.78.su@gmail.com>
2014-11-13 13:41:39 +0200

    0.1546 ADD: добавлена процедура проверки пароля )

--- pyOperaTank/pakTank/pakData/modData.py
+++ pyOperaTank/pakTank/pakData/modData.py
@@ -21,10 +21,38 @@ class clsData(object):
             """
             self.base = shelve.open('./pakTank/pakData/tank_data.db')
             
+            
+            # проверка пароля на базу данных
+            if self.base.has_key('pass'):
+                pas1 = self.base['pass']
+                pas2 = ''
+                i=0
+                while pas1 != pas2:
+                    pas2 = raw_input('Plis input password:')
+                    i += 1
+                    if i > 3:
+                        print '  [ERROR] aborting load tank_base.db'
+                        import sys
+                        sys.exit()
+            else:
+                pas1 = ''
+                pas2 = ' '
+                while pas1 != pas2 and len(pas1) < 6:
+                    print 'password mininmum 6 symbol!!!'
+                    pas1 = raw_input('Plis input password:')
+                    pas2 = raw_input('Plis confirm password:')
+                self.base['pass'] = pas1
+            
             if self.base.has_key('my_tank'):
                 self.my_tank = self.base['my_tank']
             else:
                 self.my_tank={}
+                tank={'name':'my tank',
+                      'atak': 0,
+                      'bron': 0,
+                      'toch': 0,
+                      'proch': 0,}
+            
                 self.base['my_tank'] = {}
                 self.base.sync()
         self.base = None
