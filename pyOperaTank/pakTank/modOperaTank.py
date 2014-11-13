7ab212ea3138e0268f9fd7ea39a919eaccb2b9ef
prospero78 <prospero.78.su@gmail.com>
2014-11-13 09:41:51 +0200

    0.1491 ADD: добавлен модуль с главным классом танковых игрищ. )))

---
+++ pyOperaTank/pakTank/modOperaTank.py
@@ -0,0 +1,24 @@
+# -*- coding: utf8 -*-
+"""
+Главный класс для расчёта танковых баталий.
+"""
+
+class clsOperaTank(object):
+    """
+    Это корневой сводный объект для создания взаимных связей между
+    частями программы.
+    """
+    def __init__(self):
+        """
+        Инициализация программы.
+        """
+        from pakTank.modTank import clsTank
+        self.my_tank = clsTank()
+        self.tank1 = clsTank()
+        self.tank2 = clsTank()
+        
+        from pakGui.modWinMain import clsWinMain
+        self.winMain = clsWinMain()
+        
+    def run(self):
+        self.winMain.mainloop()
