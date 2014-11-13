fd51970476ee5c7389d500ef702c6c6e60a14b32
prospero78 <prospero.78.su@gmail.com>
2014-11-13 12:03:58 +0200

    0.1533 ADD: добавлен необходимый импорт.

--- pyOperaTank/pakTank/pakGUI/modFrmRight.py
+++ pyOperaTank/pakTank/pakGUI/modFrmRight.py
@@ -5,6 +5,8 @@
 
 from Tkinter import Frame
 
+from pakTank.pakGUI.modLfrTank import clsLfrTank
+
 class clsFrmRight(Frame):
     """
     Правый фрейм содержит всякие расчётные данные по чужим танкам.
@@ -12,5 +14,5 @@ class clsFrmRight(Frame):
     def __init__(self, root=None, tank1=None, tank2=None):
         Frame.__init__(self, master=root, border=3, relief='sunken')
         self.pack(side='right', fill='y')
-        self.lfrTank1 = ClsLfrTank(root=self, label = 'Противник 1', tank=tank1)
-        self.lfrTank2 = ClsLfrTank(root=self, label = 'Противник 2', tank=tank2)
+        self.lfrTank1 = clsLfrTank(root=self, label = 'Противник 1', tank=tank1)
+        self.lfrTank2 = clsLfrTank(root=self, label = 'Противник 2', tank=tank2)
