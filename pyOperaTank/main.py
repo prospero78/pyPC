fc3e8066f27d8226d41062dbed1be00ca3d10ca2
prospero78 <prospero.78.su@gmail.com>
2014-11-13 11:11:30 +0200

    0.1507 FIX: удалён устаревший код.

--- pyOperaTank/main.py
+++ pyOperaTank/main.py
@@ -25,7 +25,7 @@ if True: # ведёт статистику по боям
     stat_los_ver = [113, 113, 114]
 
 import sys
-from Tkinter import LabelFrame, Entry, Label, Frame, Button
+from Tkinter import LabelFrame, Entry, Label, Frame
 
 class ClsKeyValue(Frame):
     """
