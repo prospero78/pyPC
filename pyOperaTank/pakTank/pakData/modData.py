f82ae9af7d5f0ad4def93b4fdc53ccc7f61be84a
prospero78 <prospero.78.su@gmail.com>
2014-11-13 14:17:07 +0200

    0.1548 ADD: добавлен метод для чтения базы по врагу № 1.

--- pyOperaTank/pakTank/pakData/modData.py
+++ pyOperaTank/pakTank/pakData/modData.py
@@ -19,42 +19,72 @@ class clsData(object):
             Читает данные из базы.
             Если её нет -- создаёт.
             """
+            def get_pass():
+                """
+                Запрашивает пароль при чтении базы, а если его нет,
+                то создаёт новый. При неуспехе ввода пароля --
+                автоматический выход.
+                """
+                if self.base.has_key('pass'):
+                    pas1 = self.base['pass']
+                    pas2 = ''
+                    i=0
+                    while pas1 != pas2:
+                        pas2 = raw_input('Plis input password:')
+                        i += 1
+                        if i > 3:
+                            print '  [ERROR] aborting load tank_base.db'
+                            import sys
+                            sys.exit()
+                else:
+                    pas1 = ''
+                    pas2 = ' '
+                    while pas1 != pas2 and len(pas1) < 6:
+                        print 'password mininmum 6 symbol!!!'
+                        pas1 = raw_input('Plis input password:')
+                        pas2 = raw_input('Plis confirm password:')
+                    self.base['pass'] = pas1
+            
+            def get_my_tank():
+                """
+                Получает данные по своему танку.
+                Если данных нет -- то заполняет нулями.
+                """
+                if self.base.has_key('my_tank'):
+                    self.my_tank = self.base['my_tank']
+                else:
+                    self.my_tank={'name':'my tank',
+                                  'atak': 0,
+                                  'bron': 0,
+                                  'toch': 0,
+                                  'proch': 0,}
+                    
+                    self.base['my_tank'] = self.my_tank
+                    self.base.sync()
+            
+            def get_tank1():
+                """
+                Получает данные по первому вражескому танку.
+                Если их нет -- автозаполнение нулями.
+                """
+                if self.base.has_key('tank1'):
+                    self.tank1 = self.base['tank1']
+                else:
+                    self.tank1={'name':'Tank #1',
+                                  'atak': 0,
+                                  'bron': 0,
+                                  'toch': 0,
+                                  'proch': 0,}
+                    
+                    self.base['tank1'] = self.tank1
+                    self.base.sync()
+                
             self.base = shelve.open('./pakTank/pakData/tank_data.db')
             
-            
-            # проверка пароля на базу данных
-            if self.base.has_key('pass'):
-                pas1 = self.base['pass']
-                pas2 = ''
-                i=0
-                while pas1 != pas2:
-                    pas2 = raw_input('Plis input password:')
-                    i += 1
-                    if i > 3:
-                        print '  [ERROR] aborting load tank_base.db'
-                        import sys
-                        sys.exit()
-            else:
-                pas1 = ''
-                pas2 = ' '
-                while pas1 != pas2 and len(pas1) < 6:
-                    print 'password mininmum 6 symbol!!!'
-                    pas1 = raw_input('Plis input password:')
-                    pas2 = raw_input('Plis confirm password:')
-                self.base['pass'] = pas1
-            
-            if self.base.has_key('my_tank'):
-                self.my_tank = self.base['my_tank']
-            else:
-                self.my_tank={}
-                tank={'name':'my tank',
-                      'atak': 0,
-                      'bron': 0,
-                      'toch': 0,
-                      'proch': 0,}
-            
-                self.base['my_tank'] = {}
-                self.base.sync()
+            get_pass()
+            get_my_tank()
+            get_tank1()
+           
         self.base = None
         base_read()
     
