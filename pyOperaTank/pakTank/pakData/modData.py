64161b7b87699eb1dd2e142f8318bd2c2b63ecd8
prospero78 <prospero.78.su@gmail.com>
2014-11-13 13:08:16 +0200

    0.1544 ADD: добавлен класс для чтения и сохранения танковых данных.

--- pyOperaTank/pakTank/pakData/modData.py
+++ pyOperaTank/pakTank/pakData/modData.py
@@ -2,3 +2,34 @@
 """
 Модуль хранения танковых данных.
 """
+
+import shelve
+
+class clsData(object):
+    """
+    Класс обеспечивает хранение, чтение, запись всех данных
+    по танковым боям.
+    """
+    def __init__(self):
+        """
+        Считывает в память все данные по боям.
+        """
+        def base_read():
+            """
+            Читает данные из базы.
+            Если её нет -- создаёт.
+            """
+            self.base = shelve.open('./pakTank/pakData/tank_data.db')
+            
+            if self.base.has_key('my_tank'):
+                self.my_tank = self.base['my_tank']
+            else:
+                self.my_tank={}
+                self.base['my_tank'] = {}
+                self.base.sync()
+        self.base = None
+        base_read()
+    
+    def close_base(self):
+        self.base.close()
+        print '   [tank_data.db close]'
