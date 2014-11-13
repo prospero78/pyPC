d82ba9dc1fcbece65beb37cdbbf3a3f092d26d35
prospero78 <prospero.78.su@gmail.com>
2014-11-13 11:57:51 +0200

    0.1529 FIX: перенос кода в другой модуль.

--- pyOperaTank/main.py
+++ pyOperaTank/main.py
@@ -27,16 +27,6 @@ if True: # ведёт статистику по боям
 import sys
 from Tkinter import Frame
 
-class ClsFrmRight(Frame):
-    """
-    Правый фрейм содержит всякие расчётные данные по чужим танкам.
-    """
-    def __init__(self, root=None, tank1=None, tank2=None):
-        Frame.__init__(self, master=root, border=3, relief='sunken')
-        self.pack(side='right', fill='y')
-        self.lfrTank1 = ClsLfrTank(root=self, label = 'Противник 1', tank=tank1)
-        self.lfrTank2 = ClsLfrTank(root=self, label = 'Противник 2', tank=tank2)
-
 def main():
     """
     Главный запускающий скрипт.
