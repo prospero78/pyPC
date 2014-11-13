f35621ca6c32c7baf0609ae98f985854463a5cb5
prospero78 <prospero.78.su@gmail.com>
2014-11-13 11:02:34 +0200

    0.1504 FIX: перенос кода в другой модуль.

--- pyOperaTank/main.py
+++ pyOperaTank/main.py
@@ -67,21 +67,6 @@ class ClsLfrTank(LabelFrame):
         self.kvBron = ClsKeyValue(root=self, key='Прочность', value=tank[3])
         self.kvMosh = ClsKeyValue(root=self, key='Мощность*', value='?')
 
-
-class ClsFrmButton(Frame):
-    """
-    Класс добавляет фрейм с кнопками для пересчёта значений.
-    """
-    def __init__(self, root=None):
-        Frame.__init__(self, master=root, border=3, relief='sunken')
-        self.pack(side='bottom', fill='x')
-        
-        self.btnExit = Button(self, text='Выход', command=lambda:(sys.exit()))
-        self.btnExit.pack(side='right')
-        
-        self.btnCalc = Button(self, text='Пересчёт')
-        self.btnCalc.pack(side='right')
-
 class ClsFrmLeft(Frame):
     """
     Левый фрейм содержит всякие расчётные данные по своему танку.
