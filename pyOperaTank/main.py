5ec7b6bb51af00c517991d0ac3351d2980777a5b
prospero78 <prospero.78.su@gmail.com>
2014-11-13 11:24:52 +0200

    0.1513 FIX: перенос кода в другой модуль.

--- pyOperaTank/main.py
+++ pyOperaTank/main.py
@@ -44,28 +44,6 @@ class ClsKeyValue(Frame):
         self.entValue = Entry(master=self)
         self.entValue.pack(side='right')
         self.entValue.insert(0, value)
-        
-
-class ClsLfrTank(LabelFrame):
-    """
-    Класс создаёт фрейм для редактирования характеристик танка.
-    """
-    def __init__(self, root=None,
-                 label='MyTank',
-                 tank=None):
-        LabelFrame.__init__(self,
-                            master=root,
-                            text = label,
-                            border=3,
-                            relief='raised',
-                            font='Consolas 12 bold')
-        self.pack(side='top', fill='y')
-        
-        self.kvAtak = ClsKeyValue(root=self, key='Атака', value=tank[0])
-        self.kvBron = ClsKeyValue(root=self, key='Броня', value=tank[1])
-        self.kvBron = ClsKeyValue(root=self, key='Точность', value=tank[2])
-        self.kvBron = ClsKeyValue(root=self, key='Прочность', value=tank[3])
-        self.kvMosh = ClsKeyValue(root=self, key='Мощность*', value='?')
 
 class ClsFrmRight(Frame):
     """
