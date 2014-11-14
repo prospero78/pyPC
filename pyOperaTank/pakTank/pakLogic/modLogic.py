df1dc2ef2a070f9fd547ba997e92feb9d68f2615
prospero78 <prospero.78.su@gmail.com>
2014-11-14 09:24:19 +0200

    0.1556 ADD: добавлена кучка методов и свойств в класс.

--- pyOperaTank/pakTank/pakLogic/modLogic.py
+++ pyOperaTank/pakTank/pakLogic/modLogic.py
@@ -2,3 +2,36 @@
 """
 Модуль для танковой логики.
 """
+
+class clsLogic(object):
+    """
+    Класс предоставляет логику приложения для правильной работы.
+    """
+    def __init__(self, root=None):
+        """
+        Создание экземпляра класса танковой логики.
+        """
+        self.root = root
+        
+    def update_gui(self):
+        """
+        Обновляет изменяемые данные в гуе.
+        """
+        
+        
+    def run(self):
+        """
+        Выполняется при запуске всей программы.
+        """
+        self.update_gui()
+        self.winMain = self.root.winMain
+        self.dbData = self.root.dbData
+        self.winMain.mainloop()
+        self.dbData.close_base()
+    
+    def stop(self):
+        """
+        Выполняется при остановке всей программы.
+        По окончании работы графики сохраняет базу данных.
+        """
+        pass
