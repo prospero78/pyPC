0e7358828c53fca4591ef2ed9bc462d7ae3c3d07
prospero78 <prospero.78.su@gmail.com>
2014-11-14 09:55:28 +0200

    0.1561 FIX/ADD: добавлены свойства класса, скрыты графические члены класса.

--- pyOperaTank/pakTank/pakGUI/modFrmKeyValue.py
+++ pyOperaTank/pakTank/pakGUI/modFrmKeyValue.py
@@ -13,12 +13,31 @@ class clsFrmKeyValue(Frame):
         Frame.__init__(self, master=root, border=3, relief='groove')
         self.pack(side='top', fill='x')
         
-        self.key=key
-        self.value=value
+        self.__key=key
+        self.__value=value
         
-        self.lblKey = Label(master=self, text=key)
-        self.lblKey.pack(side='left', fill='x')
+        self.__lblKey = Label(master=self, text=key)
+        self.__lblKey.pack(side='left', fill='x')
         
-        self.entValue = Entry(master=self)
-        self.entValue.pack(side='right')
-        self.entValue.insert(0, value)
+        self.__entValue = Entry(master=self)
+        self.__entValue.pack(side='right')
+        self.__entValue.insert(0, value)
+    
+    @property
+    def key(self):
+        return self.__key
+        
+    @key.setter
+    def key(self, value=None):
+        self.__lblKey['text'] = value
+        self.__key = value
+        
+    @property
+    def value(self):
+        return self.__value
+        
+    @value.setter
+    def value(self, value=None):
+        self.__entValue.delete(0, 'end')
+        self.__entValue.insert(0, value)
+        self.__value = value
