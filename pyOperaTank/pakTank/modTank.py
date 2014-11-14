f3f194d08f6216d9b85dd94b1eb40db8be852328
prospero78 <prospero.78.su@gmail.com>
2014-11-14 15:50:58 +0200

    0.1573 ADD: добавлен метод для динамического рачёта мощности. Мощность теперь property.

--- pyOperaTank/pakTank/modTank.py
+++ pyOperaTank/pakTank/modTank.py
@@ -19,16 +19,24 @@ class clsTank(object):
         # прочность танка
         self.proch = proch
         # общая характеристика
-        mosh_qw = 0
+        self.__mosh = 0
         # имя танка
         self.name = name
+        self.__get_mosh()
         
+    def __get_mosh(self):
+        """
+        Спец метод для расчёта мощности танка
+        """
+        
+        # расчёное значение мощности
+        mosh_qw = 0
         mosh_qw += self.atak**2
         mosh_qw += self.bron**2
         mosh_qw += self.toch**2
         mosh_qw += self.proch**2
-        self.mosh = mosh_qw**0.5
-        print 'Name:', name, '\tmosh=', int(self.mosh)
+        self.__mosh = mosh_qw**0.5
+        print 'Name:', self.name, '\tmosh=', int(self.__mosh)
         
     def get_result(self, mosh=0):
         """
@@ -42,3 +50,11 @@ class clsTank(object):
             print 'not recomended'
         else:
             print 'recomended! )))'
+            
+    @property
+    def mosh(self):
+        """
+        Расчётное свойство. Доступно только для чтения.
+        """
+        self.__get_mosh()
+        return self.__mosh 
