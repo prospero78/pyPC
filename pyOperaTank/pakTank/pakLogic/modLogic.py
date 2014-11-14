54ba5c22ceeebda051ce4e73947a1e3119da24e9
prospero78 <prospero.78.su@gmail.com>
2014-11-14 15:44:01 +0200

    0.1572 ADD: добавлен метод для загрузки оперативных данных по своему танку.

--- pyOperaTank/pakTank/pakLogic/modLogic.py
+++ pyOperaTank/pakTank/pakLogic/modLogic.py
@@ -13,6 +13,13 @@ class clsLogic(object):
         """
         self.root = root
         
+    def load_oper_data(self):
+        """
+        Загружает оперативные данные из модуля базы данных.
+        Работает при старте системы.
+        """
+        self.myTank.atak = self.dbData.myTank['atak']
+        
     def update_gui(self):
         """
         Обновляет изменяемые данные в гуе.
@@ -23,18 +30,21 @@ class clsLogic(object):
         my_tank.kvBron.value = self.myTank.bron
         my_tank.kvToch.value = self.myTank.toch
         my_tank.kvProch.value = self.myTank.proch
+        my_tank.kvMosh.value = self.myTank.mosh
         
         tank1 = self.winMain.frmRight.lfrTank1
         tank1.kvAtak.value = self.Tank1.atak
         tank1.kvBron.value = self.Tank1.bron
         tank1.kvToch.value = self.Tank1.toch
         tank1.kvProch.value = self.Tank1.proch
+        tank1.kvMosh.value = self.Tank1.mosh
         
         tank2 = self.winMain.frmRight.lfrTank2
         tank2.kvAtak.value = self.Tank2.atak
         tank2.kvBron.value = self.Tank2.bron
         tank2.kvToch.value = self.Tank2.toch
         tank2.kvProch.value = self.Tank2.proch
+        tank2.kvMosh.value = self.Tank2.mosh
         
     def run(self):
         """
@@ -46,6 +56,7 @@ class clsLogic(object):
         self.Tank1 = self.root.Tank1
         self.Tank2 = self.root.Tank2
         
+        self.load_oper_data()
         self.update_gui()
         
         self.winMain.mainloop()
