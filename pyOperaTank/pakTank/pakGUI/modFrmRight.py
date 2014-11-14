d8d88a8073cd96592c1b302689229e9ef441519e
prospero78 <prospero.78.su@gmail.com>
2014-11-14 11:13:54 +0200

    0.1564 FIX: убраны лишние параметры класса.

--- pyOperaTank/pakTank/pakGUI/modFrmRight.py
+++ pyOperaTank/pakTank/pakGUI/modFrmRight.py
@@ -11,8 +11,8 @@ class clsFrmRight(Frame):
     """
     Правый фрейм содержит всякие расчётные данные по чужим танкам.
     """
-    def __init__(self, root=None, tank1=None, tank2=None):
+    def __init__(self, root=None):
         Frame.__init__(self, master=root, border=3, relief='sunken')
         self.pack(side='right', fill='y')
-        self.lfrTank1 = clsLfrTank(root=self, label = 'Противник 1', tank=tank1)
-        self.lfrTank2 = clsLfrTank(root=self, label = 'Противник 2', tank=tank2)
+        self.lfrTank1 = clsLfrTank(root=self, label = 'Противник 1')
+        self.lfrTank2 = clsLfrTank(root=self, label = 'Противник 2')
