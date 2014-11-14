d3438174f2025869b8a2c2b84ba0ef21ce7e81b9
prospero78 <prospero.78.su@gmail.com>
2014-11-14 16:36:17 +0200

    0.1579 ADD: добавлен метод для сохранения данных по всем танкам в базу.

--- pyOperaTank/pakTank/pakLogic/modLogic.py
+++ pyOperaTank/pakTank/pakLogic/modLogic.py
@@ -34,7 +34,26 @@ class clsLogic(object):
         self.Tank2.toch = self.dbData.Tank2['toch']
         self.Tank2.proch = self.dbData.Tank2['proch']
         
+    
+    def save_oper_data(self):
+        """
+        Сохраняет оперативные данные в базу данных.
+        """
+        self.dbData.myTank['atak'] = self.myTank.atak
+        self.dbData.myTank['bron'] = self.myTank.bron
+        self.dbData.myTank['toch'] = self.myTank.toch
+        self.dbData.myTank['proch']= self.myTank.proch
         
+        self.dbData.Tank1['atak'] = self.Tank1.atak
+        self.dbData.Tank1['bron'] = self.Tank1.bron
+        self.dbData.Tank1['toch'] = self.Tank1.toch
+        self.dbData.Tank1['proch']= self.Tank1.proch
+        
+        self.dbData.Tank2['atak'] = self.Tank1.atak
+        self.dbData.Tank2['bron'] = self.Tank1.bron
+        self.dbData.Tank2['toch'] = self.Tank1.toch
+        self.dbData.Tank2['proch']= self.Tank1.proch
+    
     def update_gui(self):
         """
         Обновляет изменяемые данные в гуе.
