f9f0b28276a571e04c65e0a0a4f0513edec2295e
prospero78 <prospero.78.su@gmail.com>
2014-11-14 09:40:44 +0200

    0.1558 FIX/ADD: перемещёе порядок действий при запуске, добавлен код для обновления полей данных.

--- pyOperaTank/pakTank/pakLogic/modLogic.py
+++ pyOperaTank/pakTank/pakLogic/modLogic.py
@@ -17,15 +17,23 @@ class clsLogic(object):
         """
         Обновляет изменяемые данные в гуе.
         """
+        # --- обновления по своему танку ---
+        my_tank = self.winMain.frmLeft.lfrMyTank
+        my_tank.kvAtak.entValue.delete(0, 'end')
+        my_tank.kvAtak.entValue.insert(0, self.dbData.my_tank['atak'])
         
+        my_tank.kvBron.entValue.delete(0, 'end')
+        my_tank.kvBron.entValue.insert(0, self.dbData.my_tank['bron'])
         
     def run(self):
         """
         Выполняется при запуске всей программы.
         """
-        self.update_gui()
-        self.winMain = self.root.winMain
         self.dbData = self.root.dbData
+        self.winMain = self.root.winMain
+        
+        self.update_gui()
+        
         self.winMain.mainloop()
         self.dbData.close_base()
     
