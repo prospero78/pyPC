c227c043e5d38b3d690ba3aa5298dc57c850c1da
prospero78 <prospero.78.su@gmail.com>
2014-11-15 19:56:48 +0200

    0.1590 ADD: добавлена ссылка на функцию обратного вызова для сохранения данных в базе.

--- pyOperaTank/pakTank/pakGUI/modLfrTank.py
+++ pyOperaTank/pakTank/pakGUI/modLfrTank.py
@@ -12,7 +12,8 @@ class clsLfrTank(LabelFrame):
     Класс создаёт фрейм для редактирования характеристик танка.
     """
     def __init__(self, root=None,
-                 label='MyTank'):
+                 label='MyTank',
+                 clb=None):
         LabelFrame.__init__(self,
                             master=root,
                             text = label,
@@ -21,8 +22,8 @@ class clsLfrTank(LabelFrame):
                             font='Consolas 12 bold')
         self.pack(side='top', fill='x')
         
-        self.kvAtak = clsFrmKeyValue(root=self, key='Атака', value=0)
-        self.kvBron = clsFrmKeyValue(root=self, key='Броня', value=0)
-        self.kvToch = clsFrmKeyValue(root=self, key='Точность', value=0)
-        self.kvProch = clsFrmKeyValue(root=self, key='Прочность', value=0)
-        self.kvMosh = clsFrmKeyValue(root=self, key='[Мощность]', value=0)
+        self.kvAtak = clsFrmKeyValue(root=self, key='Атака', value=0, clb=clb)
+        self.kvBron = clsFrmKeyValue(root=self, key='Броня', value=0, clb=clb)
+        self.kvToch = clsFrmKeyValue(root=self, key='Точность', value=0, clb=clb)
+        self.kvProch = clsFrmKeyValue(root=self, key='Прочность', value=0, clb=clb)
+        self.kvMosh = clsFrmKeyValue(root=self, key='[Мощность]', value=0, clb=clb)
