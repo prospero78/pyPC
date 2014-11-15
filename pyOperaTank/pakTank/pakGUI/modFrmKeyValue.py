7c7591b130460dc87a8ea30ef8b3d5d846f33c00
prospero78 <prospero.78.su@gmail.com>
2014-11-15 19:56:03 +0200

    0.1587 ADD: добавлены события для обработки кнопки сохранения, добавлена функция обратного вызова для сохранения данных в базе.

--- pyOperaTank/pakTank/pakGUI/modFrmKeyValue.py
+++ pyOperaTank/pakTank/pakGUI/modFrmKeyValue.py
@@ -9,12 +9,13 @@ class clsFrmKeyValue(Frame):
     """
     Класс обеспечивает пару "ключ:значение"
     """
-    def __init__(self, root=None, key='key', value='None'):
+    def __init__(self, root=None, key='key', value='None', clb=None):
         Frame.__init__(self, master=root, border=3, relief='groove')
         self.pack(side='top', fill='both', expand=1)
         
         self.__key=key
         self.__value=value
+        self.callback = clb
         
         self.__lblKey = Label(master=self, text=key, anchor='e')
         self.__lblKey.pack(side='left', fill='x', expand=1)
@@ -26,7 +27,20 @@ class clsFrmKeyValue(Frame):
         
         self.__btnReset = Button(self, text='R', border=4, relief='groove', fg='blue', command=self.__btnReset_click)
         self.__btnReset.pack(side='left')
+        
+        self.__btnSave = Button(self, text='S', border=4, relief='groove', fg='blue', command=self.__btnSave_click, state='disabled')
+        self.__btnSave.pack(side='left')
     
+    def __btnSave_click(self, event=None):
+        """
+        При нажатии на кнопку сохранить происходит сохранение в файл.
+        """
+        txt = self.__entValue.get()
+        self.__value = txt
+        self.callback()
+        self.__btnSave['fg'] = 'blue'
+        self.__btnSave['state'] = 'disabled'
+        
     def __btnReset_click(self, event=None):
         """
         При нажатии на кнопку сброс происходит сброс значения value.
@@ -41,8 +55,12 @@ class clsFrmKeyValue(Frame):
         txt = self.__entValue.get()
         if txt != str(self.__value):
             self.__btnReset['fg'] = 'red'
+            self.__btnSave['fg'] = 'red'
+            self.__btnSave['state'] = 'normal'
         else:
             self.__btnReset['fg'] = 'blue'
+            self.__btnSave['fg'] = 'blue'
+            self.__btnSave['state'] = 'disabled'
         self.__entValue.after(100, self.__control_change)
     
     @property
